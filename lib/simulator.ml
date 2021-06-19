b818e88d06a0d51cf6a065d432c72363f15973d9
Patrik Keller <git@pkel.dev>
2021-06-19 21:00:39 +0200

    Nakamoto: support out-of-order delivery of blocks

--- lib/simulator.ml
+++ lib/simulator.ml
@@ -46,6 +46,36 @@ module Nakamoto = struct
     | [ p ] -> child.height = p.height + 1
     | _ -> false
 
+  let have_common_ancestor view =
+    let rec h a b =
+      if a == b then true
+      else
+        let a' = Dag.data view a and b' = Dag.data view b in
+        if a'.height = b'.height then
+          match Dag.parents view a, Dag.parents view b with
+          | [ a ], [ b ] -> h a b
+          | [], _
+          | _, [] -> false
+          | _ -> failwith "invalid dag"
+        else if a'.height > b'.height then
+          match Dag.parents view a with
+          | [ a ] -> h a b
+          | [] -> false
+          | _ -> failwith "invalid dag"
+        else
+          match Dag.parents view b with
+          | [ b ] -> h a b
+          | [] -> false
+          | _ -> failwith "invalid dag"
+    in h
+
+  let leaves view gnode =
+    let rec h acc gnode =
+      match Dag.children view gnode with
+      | [] -> gnode :: acc
+      | l -> List.fold_left h acc l
+    in h [] gnode
+
   let event_handler ctx preferred = function
     | Activate ->
       let head = Dag.data ctx.view preferred in
@@ -53,13 +83,18 @@ module Nakamoto = struct
       ctx.release head';
       head'
     | Deliver gnode ->
-      (* TODO: Preference can break when order of messages is off. *)
-      (* TODO: Only prefer gnode if its heritage is visible. *)
-      (* TODO: Consider gnode's offspring. *)
-      let node = Dag.data ctx.view gnode
-      and head = Dag.data ctx.view preferred in
-      if node.height > head.height then
-        gnode
+      (* Only consider gnode if its heritage is visible. *)
+      if have_common_ancestor ctx.view gnode preferred then
+        let consider preferred gnode =
+          let node = Dag.data ctx.view gnode
+          and head = Dag.data ctx.view preferred in
+          if node.height > head.height then
+            gnode
+          else
+            preferred
+        in
+        (* delayed gnode might connect nodes delivered previously *)
+        List.fold_left consider preferred (leaves ctx.view gnode)
       else
         preferred
 
