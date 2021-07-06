26023c5172135f7800f9039244419cc531e50415
Patrik Keller <git@pkel.dev>
2021-07-06 16:13:26 +0200

    Parallel execution of simulations
    Most of the diff on experiments/honest_net.ml is due to the fact that
    Parany does not allow to Marshal/demux closures. Previously, my list of
    tasks had closures for protocol and reward functions. Now, the list of
    tasks is data only.

--- experiments/honest_net.ml
+++ experiments/honest_net.ml
@@ -1,205 +1,248 @@
 open Cpr_lib
 open Cpr_protocols
 
-type 'a with_tag_and_description =
-  { tag : string
-  ; description : string
-  ; it : 'a
-  }
+module Task = struct
+  type network = Simple10uniform
 
-let networks =
-  [ { tag = "simple10-uni"
-    ; description =
-        "10 nodes, compute 1..10, simple dissemination, uniform delay 0.6..1.4"
-    ; it =
-        (let delay = Distributions.uniform ~lower:0.6 ~upper:1.4 in
-         Network.homogeneous ~delay 10
-         |> fun n ->
-         { n with
-           nodes =
-             Array.mapi
-               Network.(fun i x -> { x with compute = float_of_int (i + 1) })
-               n.nodes
-         })
-    }
-  ]
-;;
+  let tag_network = function
+    | Simple10uniform -> "simple10-uni"
+  ;;
 
-type protocol =
-  | P :
-      { consensus : ('a Simulator.data, 'a, 'b, Simulator.pow) Protocol.protocol
-      ; k : int (* pow per block *)
-      ; incentive_schemes :
-          ('a Simulator.data, 'a) Protocol.reward_function with_tag_and_description list
+  let describe_network = function
+    | Simple10uniform ->
+      "10 nodes, compute 1..10, simple dissemination, uniform delay 0.6..1.4"
+  ;;
+
+  let create_network = function
+    | Simple10uniform ->
+      let delay = Distributions.uniform ~lower:0.6 ~upper:1.4 in
+      Network.homogeneous ~delay 10
+      |> fun n ->
+      { n with
+        nodes =
+          Array.mapi
+            Network.(fun i x -> { x with compute = float_of_int (i + 1) })
+            n.nodes
       }
-      -> protocol
+  ;;
 
-let nakamoto =
-  { tag = "nakamoto"
-  ; description = "Nakamoto consensus"
-  ; it =
-      P
+  type protocol =
+    | Nakamoto
+    | B_k_lessleadership of { k : int }
+    | George of { k : int }
+
+  let protocol_family = function
+    | Nakamoto -> "nakamoto"
+    | B_k_lessleadership _ -> "bk"
+    | George _ -> "george"
+  ;;
+
+  let pow_per_block = function
+    | Nakamoto -> 1
+    | B_k_lessleadership { k } | George { k } -> k
+  ;;
+
+  let describe_protocol = function
+    | Nakamoto -> "Nakamoto consensus"
+    | B_k_lessleadership { k } ->
+      "Bₖ with less leader modification and k=" ^ string_of_int k
+    | George { k } -> "George's protocol with k=" ^ string_of_int k
+  ;;
+
+  type incentive_scheme =
+    | Constant
+    | Discount
+    | Punish
+
+  let tag_incentive_scheme = function
+    | Constant -> "constant"
+    | Discount -> "discount"
+    | Punish -> "punish"
+  ;;
+
+  let describe_incentive_scheme = function
+    | Constant -> "1 per confirmed block, divided equally among confirmed pow"
+    | Discount ->
+      "max 1 per confirmed block, divided equally among confirmed pow, discount for \
+       non-linearity"
+    | Punish ->
+      "max 1 per confirmed block, 1/k per confirmed pow on the longest chain of votes"
+  ;;
+
+  type model =
+    | M :
+        { consensus : ('a Simulator.data, 'a, 'b, Simulator.pow) Protocol.protocol
+        ; reward_functions : ('a Simulator.data, 'a) Protocol.reward_function list
+        }
+        -> model
+
+  let model protocol incentive_schemes =
+    let fail x =
+      let msg =
+        Printf.sprintf
+          "protocol \"%s\" does not support incentive scheme \"%s\""
+          (protocol_family protocol)
+          (tag_incentive_scheme x)
+      in
+      failwith msg
+    in
+    match protocol with
+    | Nakamoto ->
+      M
         { consensus = Nakamoto.protocol
-        ; k = 1
-        ; incentive_schemes =
-            [ { tag = "constant"
-              ; description = "1 per confirmed block"
-              ; it = Nakamoto.constant_reward 1.
-              }
-            ]
+        ; reward_functions =
+            List.map
+              (function
+                | Constant -> Nakamoto.constant_reward 1.
+                | x -> fail x)
+              incentive_schemes
         }
-  }
-;;
-
-let bk k =
-  { tag = "bk"
-  ; description = "Bₖ with less leader modification and k=" ^ string_of_int k
-  ; it =
-      P
+    | B_k_lessleadership { k } ->
+      M
         { consensus = B_k_lessleader.protocol ~k
-        ; k
-        ; incentive_schemes =
-            [ { tag = "constant"
-              ; description =
-                  "1 per confirmed block, split equally between referenced votes"
-              ; it = B_k_lessleader.constant_reward_per_pow (1. /. float_of_int k)
-              }
-            ]
+        ; reward_functions =
+            List.map
+              (function
+                | Constant -> B_k_lessleader.constant_reward_per_pow (1. /. float_of_int k)
+                | x -> fail x)
+              incentive_schemes
         }
-  }
-;;
-
-let george k =
-  { tag = "george"
-  ; description = "George's protocol with k=" ^ string_of_int k
-  ; it =
-      P
+    | George { k } ->
+      M
         { consensus = George.protocol ~k
-        ; k
-        ; incentive_schemes =
-            [ { tag = "constant"
-              ; description =
-                  "1 per confirmed block, split equally between referenced pow solutions"
-              ; it = George.constant_reward_per_pow ~reward_per_block:1. ~k
-              }
-            ; { tag = "discount"
-              ; description =
-                  "max 1 per confirmed block, discount for non-linearity, split equally \
-                   between referenced pow solutions"
-              ; it = George.discount_vote_depth ~max_reward_per_block:1. ~k
-              }
-            ; { tag = "punish"
-              ; description =
-                  "max 1 per confirmed block, 1/k per pow solution on the longest chain \
-                   of pow solutions"
-              ; it = George.punish_nonlinear ~max_reward_per_block:1. ~k
-              }
-            ]
+        ; reward_functions =
+            List.map
+              (function
+                | Constant -> George.constant_reward_per_pow ~reward_per_block:1. ~k
+                | Discount -> George.discount_vote_depth ~max_reward_per_block:1. ~k
+                | Punish -> George.punish_nonlinear ~max_reward_per_block:1. ~k)
+              incentive_schemes
         }
-  }
-;;
+  ;;
+
+  type t =
+    { network : network
+    ; protocol : protocol
+    ; incentive_schemes : incentive_scheme list
+    ; activations : int
+    ; activation_delay : float
+    }
+
+  let to_string t =
+    Printf.sprintf
+      "%s/k=%i/%gs/%s"
+      (protocol_family t.protocol)
+      (pow_per_block t.protocol)
+      t.activation_delay
+      (tag_network t.network)
+  ;;
+end
+
+let networks = [ Task.Simple10uniform ]
 
 let protocols =
   let k = [ 1; 2; 4; 8; 16; 32; 64; 128 ] in
-  List.concat [ [ nakamoto ]; List.map bk k; List.map george k ]
+  let open Task in
+  List.concat
+    [ [ Task.Nakamoto, [ Constant ] ]
+    ; List.map (fun k -> Task.B_k_lessleadership { k }, [ Constant ]) k
+    ; List.map (fun k -> Task.George { k }, [ Constant; Punish; Discount ]) k
+    ]
 ;;
 
 let block_intervals = [ 30.; 60.; 120.; 300.; 600. ]
 
-let simulations =
+let tasks activations =
   List.concat_map
     (fun network ->
       List.concat_map
-        (fun protocol ->
+        (fun (protocol, incentive_schemes) ->
           List.map
-            (fun block_interval -> network, protocol, block_interval)
+            (fun block_interval ->
+              let open Task in
+              { network
+              ; incentive_schemes
+              ; protocol
+              ; activations
+              ; activation_delay =
+                  block_interval /. (Task.pow_per_block protocol |> float_of_int)
+              })
             block_intervals)
         protocols)
     networks
 ;;
 
-let df =
-  Owl_dataframe.make
-    [| "network"
-     ; "network_description"
-     ; "compute"
-     ; "protocol"
-     ; "k"
-     ; "protocol_description"
-     ; "block_interval"
-     ; "activation_delay"
-     ; "number_activations"
-     ; "activations"
-     ; "incentive_scheme"
-     ; "incentive_scheme_description"
-     ; "reward"
-    |]
-;;
+type row =
+  { network : string
+  ; network_description : string
+  ; compute : float array
+  ; protocol : string
+  ; k : int
+  ; protocol_description : string
+  ; block_interval : float
+  ; activation_delay : float
+  ; number_activations : int
+  ; activations : int array
+  ; incentive_scheme : string
+  ; incentive_scheme_description : string
+  ; reward : float array
+  }
 
-let record
-    ~params
-    ~network
-    ~compute
-    ~protocol
-    ~block_interval
-    ~incentive_scheme
-    ~reward
-    ~activations
-  =
-  let open Simulator in
+let df_spec =
   let open Owl_dataframe in
-  let (P protocol') = protocol.it in
   let array f arr = String (Array.to_list arr |> List.map f |> String.concat "|") in
-  append_row
-    df
-    [| String network.tag
-     ; String network.description
-     ; array string_of_float compute
-     ; String protocol.tag
-     ; Int protocol'.k
-     ; String protocol.description
-     ; Float block_interval
-     ; Float params.activation_delay
-     ; Int params.activations
-     ; array string_of_int activations
-     ; String incentive_scheme.tag
-     ; String incentive_scheme.description
-     ; array string_of_float reward
-    |]
+  [| ("network", fun row -> String row.network)
+   ; ("network_description", fun row -> String row.network_description)
+   ; ("compute", fun row -> array string_of_float row.compute)
+   ; ("protocol", fun row -> String row.protocol)
+   ; ("k", fun row -> Int row.k)
+   ; ("protocol_description", fun row -> String row.protocol_description)
+   ; ("block_interval", fun row -> Float row.block_interval)
+   ; ("activation_delay", fun row -> Float row.activation_delay)
+   ; ("number_activations", fun row -> Int row.number_activations)
+   ; ("activations", fun row -> array string_of_int row.activations)
+   ; ("incentive_scheme", fun row -> String row.incentive_scheme)
+   ; ("incentive_scheme_description", fun row -> String row.incentive_scheme_description)
+   ; ("reward", fun row -> array string_of_float row.reward)
+  |]
 ;;
 
-let run activations (network, protocol, block_interval) =
-  let (P p) = protocol.it in
-  let () =
-    Printf.eprintf "%s/k=%i/%gs/%s\n%!" protocol.tag p.k block_interval network.tag
+let save_rows_as_tsv filename l =
+  let df = Owl_dataframe.make (Array.map fst df_spec) in
+  let record (row : row) =
+    Array.map (fun (_, f) -> f row) df_spec |> Owl_dataframe.append_row df
   in
+  List.iter record l;
+  Owl_dataframe.to_csv ~sep:'\t' df filename
+;;
+
+let run task =
+  let open Task in
   (* network stats *)
+  let network = create_network task.network in
   let compute =
     let open Network in
-    Array.map (fun x -> x.compute) network.it.nodes
+    Array.map (fun x -> x.compute) network.nodes
   in
   (* simulate *)
   let open Simulator in
   let params =
-    { network = network.it
-    ; activations
-    ; activation_delay = block_interval /. float_of_int p.k
-    }
+    { network; activations = task.activations; activation_delay = task.activation_delay }
   in
-  init params p.consensus
+  let (M m) = model task.protocol task.incentive_schemes in
+  init params m.consensus
   |> loop params
   |> fun sim ->
   let activations = Array.map (fun x -> x.n_activations) sim.nodes in
   Array.to_seq sim.nodes
-  |> Seq.map (fun x -> p.consensus.head x.state)
+  |> Seq.map (fun x -> m.consensus.head x.state)
   |> Dag.common_ancestor' sim.global_view
   |> function
   | None -> failwith "no common ancestor found"
   | Some common_chain ->
     (* incentive stats *)
-    List.iter
-      (fun is ->
-        let rewardfn = is.it in
+    List.map2
+      (fun is rewardfn ->
         let reward = Array.make (Array.length sim.nodes) 0. in
         rewardfn
           sim.global_view
@@ -207,22 +250,42 @@ let run activations (network, protocol, block_interval) =
           (fun x -> x.appended_by)
           common_chain
           reward;
-        record
-          ~params
-          ~activations
-          ~compute
-          ~network
-          ~protocol
-          ~block_interval
-          ~incentive_scheme:is
-          ~reward)
-      p.incentive_schemes
+        { network = tag_network task.network
+        ; network_description = describe_network task.network
+        ; protocol = protocol_family task.protocol
+        ; protocol_description = describe_protocol task.protocol
+        ; k = pow_per_block task.protocol
+        ; activation_delay = task.activation_delay
+        ; number_activations = task.activations
+        ; activations
+        ; compute
+        ; block_interval =
+            task.activation_delay *. (pow_per_block task.protocol |> float_of_int)
+        ; incentive_scheme = tag_incentive_scheme is
+        ; incentive_scheme_description = describe_incentive_scheme is
+        ; reward
+        })
+      task.incentive_schemes
+      m.reward_functions
 ;;
 
-let main n_activations filename =
-  Printf.eprintf "Simulate %d configurations...\n%!" (List.length simulations);
-  List.iter (run n_activations) simulations;
-  Owl_dataframe.to_csv ~sep:'\t' df filename
+let main n_activations n_cores filename =
+  let tasks = tasks n_activations in
+  Printf.eprintf "Simulate %d configurations...\n%!" (List.length tasks);
+  let queue = ref tasks in
+  let acc = ref [] in
+  Parany.run
+    n_cores
+    ~demux:(fun () ->
+      match !queue with
+      | [] -> raise Parany.End_of_input
+      | hd :: tl ->
+        queue := tl;
+        hd)
+    ~work:(fun task -> run task)
+    ~mux:(fun l -> acc := l :: !acc);
+  let rows = List.concat (List.rev !acc) in
+  save_rows_as_tsv filename rows
 ;;
 
 open Cmdliner
@@ -230,7 +293,13 @@ open Cmdliner
 let activations =
   let doc = "Number of proof-of-work activations simulated per output row." in
   let env = Arg.env_var "CPR_ACTIVATIONS" ~doc in
-  Arg.(value & opt int 10000 & info [ "n" ] ~env ~docv:"ACTIVATIONS" ~doc)
+  Arg.(value & opt int 10000 & info [ "n"; "activations" ] ~env ~docv:"ACTIVATIONS" ~doc)
+;;
+
+let cores =
+  let doc = "Number of simulation tasks run in parallel" in
+  let env = Arg.env_var "CPR_CORES" ~doc in
+  Arg.(value & opt int (Cpu.numcores ()) & info [ "p"; "cores" ] ~env ~docv:"CORES" ~doc)
 ;;
 
 let filename =
@@ -238,7 +307,7 @@ let filename =
   Arg.(required & pos 0 (some string) None & info [] ~docv:"OUTPUT" ~doc)
 ;;
 
-let main_t = Term.(const main $ activations $ filename)
+let main_t = Term.(const main $ activations $ cores $ filename)
 
 let info =
   let doc = "simulate various protocols in honest network" in
