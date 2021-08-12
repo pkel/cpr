dcdcfb2e739f3720665f81e4e0b191b27a863463
Jakob Hollenstein <517680+jkbjh@users.noreply.github.com>
2021-08-12 14:55:31 +0200

    test to trigger segfault bug in honest/selfish policy.

--- python/gym/tests/test_brigde.py
+++ python/gym/tests/test_brigde.py
@@ -6,3 +6,11 @@ def test_engine():
     obs = engine.reset(env)
     obs, rew, done, info = engine.step(env, 0)
     assert not done
+
+
+def test_engine_600steps():  # see if the engine works for 600 steps, the policy failed.
+    env = engine.create(specs.default)
+    obs = engine.reset(env)
+    for i in range(600):
+        obs, rew, done, info = engine.step(env, 3)
+    assert not done
--- python/gym/tests/test_specs.py
+++ python/gym/tests/test_specs.py
@@ -1,5 +1,6 @@
-import gym
 import numpy as np
+
+import gym
 from cpr_gym import specs
 
 
@@ -17,9 +18,17 @@ def test_spec_arg(capsys):
     assert captured == "Protocol Bₖ with k=8 against α=0.33 attacker"
 
 
-def test_policies():
+def test_policies_honest():
+    env = gym.make("cpr-v0", spec=specs.bk(k=8, alpha=0.33))
+    p = env.policies()["honest"]
+    obs = env.reset()
+    for x in range(600):
+        obs, _, _, _ = env.step(p(np.array(obs)))
+
+
+def test_policies_selfish():
     env = gym.make("cpr-v0", spec=specs.bk(k=8, alpha=0.33))
     p = env.policies()["selfish"]
     obs = env.reset()
-    for x in range(100):
+    for x in range(600):
         obs, _, _, _ = env.step(p(np.array(obs)))
