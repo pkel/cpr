ec85caa2dfd65f3b980440dc2c4d913222ee6a3e
Patrik Keller <git@pkel.dev>
2021-10-16 17:34:14 +0200

    iGraph experiment: collect and store output files

--- ocaml/experiments/igraph.ml
+++ ocaml/experiments/igraph.ml
@@ -35,52 +35,61 @@ let run ~activations ~srcfile ~protocol =
     |> Dag.common_ancestor' env.global.view
     |> Option.get
   in
-  R.trap_exn
-    (Collection.iter (fun rewardfn ->
-         let name, ext = Fpath.(split_ext (base srcfile)) in
-         let dstfile =
-           Fpath.(
-             outdir
-             / strf
-                 "%a-%s-%i-%s%s"
-                 pp
-                 name
-                 protocol.key
-                 protocol.pow_per_block
-                 rewardfn.key
-                 ext)
-         in
-         let rewards = Simulator.apply_reward_function rewardfn.it head env in
-         let open GraphML.Data.Write in
-         let graph_data =
-           [ "activations", int activations
-           ; "source_graph", relativize srcfile |> Fpath.to_string |> string
-           ; "version", string version
-           ; "protocol", string protocol.key
-           ; "protocol_info", string protocol.info
-           ; "pow_per_block", int protocol.pow_per_block
-           ; "reward_function", string rewardfn.key
-           ; "reward_function_info", string rewardfn.info
-           ; "ca_time", float (Dag.data head).appended_at
-           ; "ca_height", int (protocol.height (Dag.data head).value)
-           ; "machine_duration", float machine_duration
-           ]
-         in
-         let node_data i =
-           let (Node n) = env.nodes.(i) in
-           [ "reward", float rewards.(i); "activations", int n.n_activations ]
-         in
-         let g = to_graphml ~node_data ~graph_data () in
-         GraphML.write_graph g dstfile |> R.failwith_error_msg))
-    protocol.reward_functions
-  |> R.error_exn_trap_to_msg
+  let dstfiles =
+    Collection.map_to_list
+      (fun rewardfn ->
+        let name, ext = Fpath.(split_ext (base srcfile)) in
+        let dstfile =
+          Fpath.(
+            outdir
+            / strf
+                "%a-%s-%i-%s%s"
+                pp
+                name
+                protocol.key
+                protocol.pow_per_block
+                rewardfn.key
+                ext)
+        in
+        let rewards = Simulator.apply_reward_function rewardfn.it head env in
+        let open GraphML.Data.Write in
+        let graph_data =
+          [ "activations", int activations
+          ; "source_graph", relativize srcfile |> Fpath.to_string |> string
+          ; "version", string version
+          ; "protocol", string protocol.key
+          ; "protocol_info", string protocol.info
+          ; "pow_per_block", int protocol.pow_per_block
+          ; "reward_function", string rewardfn.key
+          ; "reward_function_info", string rewardfn.info
+          ; "ca_time", float (Dag.data head).appended_at
+          ; "ca_height", int (protocol.height (Dag.data head).value)
+          ; "machine_duration", float machine_duration
+          ]
+        in
+        let node_data i =
+          let (Node n) = env.nodes.(i) in
+          [ "reward", float rewards.(i); "activations", int n.n_activations ]
+        in
+        let g = to_graphml ~node_data ~graph_data () in
+        let+ () = GraphML.write_graph g dstfile in
+        dstfile)
+      protocol.reward_functions
+  in
+  List.fold_left
+    (fun acc el ->
+      let+ acc = acc
+      and+ el = el in
+      el :: acc)
+    (Ok [])
+    dstfiles
 ;;
 
-(* TODO: Write list of output graphs to txt file *)
 (* TODO: Ensure unique output file names with digest? *)
 
 let run_all activations cores =
   let open ResultSyntax in
+  (* one task per combination of network file and protocol *)
   let* networks =
     OS.Dir.contents inpdir
     >>| List.filter (fun path -> OS.File.exists path |> Result.value ~default:false)
@@ -91,7 +100,9 @@ let run_all activations cores =
       (fun network -> List.map (fun protocol -> network, protocol) protocols)
       networks
   in
+  (* process the input files in parallel *)
   let queue = ref tasks
+  and dstfiles = ref []
   and all_ok = ref true in
   Progress.with_reporter
     (Common.progress_bar (List.length tasks))
@@ -100,13 +111,37 @@ let run_all activations cores =
         cores
         ~demux:(Common.parany_demux_list_ref queue)
         ~work:(fun (srcfile, protocol) ->
-          run ~activations ~srcfile ~protocol
+          R.trap_exn (fun () -> run ~activations ~srcfile ~protocol) ()
+          |> R.error_exn_trap_to_msg
+          |> R.join
           |> R.reword_error_msg (R.msgf "ERROR: %a: %s" Fpath.pp (relativize srcfile)))
         ~mux:(fun r ->
           progress 1;
-          R.ignore_error r ~use:(fun e ->
-              all_ok := false;
-              R.pp_msg Fmt.stderr e)));
+          let l =
+            R.ignore_error r ~use:(fun e ->
+                all_ok := false;
+                R.pp_msg Fmt.stderr e;
+                [])
+          in
+          dstfiles := l @ !dstfiles));
+  (* write recent output files to file *)
+  let* () =
+    OS.File.with_oc
+      Fpath.(outdir / "last-run.txt")
+      (fun oc () ->
+        List.rev !dstfiles
+        |> List.iter (fun f ->
+               let rel =
+                 Fpath.relativize ~root:outdir f
+                 |> Option.value ~default:(relativize f)
+                 |> Fpath.to_string
+               in
+               Printf.fprintf oc "%s\n" rel)
+        |> R.ok)
+      ()
+    |> R.failwith_error_msg
+  in
+  (* return error code if something failed before *)
   if !all_ok then Ok () else R.error_msgf "some tasks failed"
 ;;
 
