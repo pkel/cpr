7e08b21969530bd8e6fe4683903e90941a34b906
Ben Glickenhaus <benglickenhaus@gmail.com>
2022-04-24 10:51:49 -0400

    save best model callback

--- rl/scripts/train.py
+++ rl/scripts/train.py
@@ -30,19 +30,53 @@ from stable_baselines3.common.monitor import Monitor
 import wandb
 
 
-class LoggingCallback(BaseCallback):
+class SaveOnBestTrainingRewardCallback(BaseCallback):
     """
-    Custom callback for plotting additional values in tensorboard.
+    Callback for saving a model (the check is done every ``check_freq`` steps)
+    based on the training reward (in practice, we recommend using ``EvalCallback``).
+
+    :param check_freq:
+    :param log_dir: Path to the folder where the model will be saved.
+      It must contains the file created by the ``Monitor`` wrapper.
+    :param verbose: Verbosity level.
     """
 
-    def __init__(self, verbose=0):
-        super(LoggingCallback, self).__init__(verbose)
+    def __init__(self, check_freq: int, log_dir: str, verbose: int = 1):
+        super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.log_dir = log_dir
+        self.save_path = os.path.join(log_dir, "best_model")
+        self.best_mean_reward = -np.inf
+
+    def _init_callback(self) -> None:
+        # Create folder if needed
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
 
     def _on_step(self) -> bool:
-        # Log scalar value (here a random variable)
-        x, y = ts2xy(load_results(log_dir), "timesteps")
-        mean_reward = np.mean(y[-100:])
-        wandb.log({"mean_reward": mean_reward})
+        if self.n_calls % self.check_freq == 0:
+
+            # Retrieve training reward
+            x, y = ts2xy(load_results(self.log_dir), "timesteps")
+            if len(x) > 0:
+                # Mean training reward over the last 100 episodes
+                mean_reward = np.mean(y[-1000:])
+                wandb.log({"mean_reward": mean_reward})
+
+                if self.verbose > 0:
+                    print(f"Num timesteps: {self.num_timesteps}")
+                    print(
+                        f"Best mean reward: {self.best_mean_reward:.2f} - Last mean reward per episode: {mean_reward:.2f}"
+                    )
+
+                # New best model, you could save the agent here
+                if mean_reward > self.best_mean_reward:
+                    self.best_mean_reward = mean_reward
+                    # Example for saving best model
+                    if self.verbose > 0:
+                        print(f"Saving new best model to {self.save_path}")
+                    self.model.save(self.save_path)
+
         return True
 
 
@@ -65,12 +99,12 @@ def env_fn(alpha):
 
 config = dict(
     # ALPHA=0.35,
-    ALGO="DQN",
+    ALGO="PPO",
     TOTAL_TIMESTEPS=10e6,
     STEPS_PER_ROLLOUT=250,
     STARTING_LR=10e-4,
     ENDING_LR=10e-7,
-    BATCH_SIZE=100000,
+    BATCH_SIZE=2048,
     ALPHA_SCHEDULE_CUTOFF=0,
     LAYER_SIZE=100,
     N_LAYERS=2,
@@ -153,44 +187,12 @@ elif config["ALGO"] == "DQN":
         exploration_initial_eps=config["STARTING_EPS"],
         exploration_final_eps=config["ENDING_EPS"],
     )
-    # replay_buffer = model.replay_buffer
-    # actions = []
-    # new_obs = []
-    # rewards = []
-    # dones = []
-    # infos = []
-    # i = 0
-    # while i < model.learning_starts * config["N_STEPS_MULTIPLE"]:
-    #     obs = env.reset()
-    #     done = False
-    #     while not done:
-    #         actions = []
-    #         new_obs = []
-    #         rewards = []
-    #         dones = []
-    #         infos = []
-    #         if obs[1] > obs[0]:
-    #             action = 1
-    #         elif obs[1] < obs[0]:
-    #             action = 0
-    #         else:
-    #             action = 3
-    #         obs, reward, done, info = env.step(action)
-    #         actions.append(action)
-    #         new_obs.append(obs)
-    #         rewards.append(reward)
-    #         dones.append(done)
-    #         infos.append(info)
-    #     actions = np.array(actions)
-    #     new_obs = np.array(new_obs)
-    #     rewards = np.array(rewards)
-    #     dones = np.array(dones)
-    #     infos = np.array(infos)
-
-    #     model._store_transition(replay_buffer, actions, new_obs, rewards, dones, infos)
-    #     i += 1
-    # model.load_replay_buffer()
-model.learn(total_timesteps=config["TOTAL_TIMESTEPS"], callback=LoggingCallback())
+model.learn(
+    total_timesteps=config["TOTAL_TIMESTEPS"],
+    callback=SaveOnBestTrainingRewardCallback(
+        check_freq=1000, log_dir=log_dir, verbose=0
+    ),
+)
 model.save(os.path.join(wandb.run.dir, f"{config['ALGO']}_nakamoto"))
 
 # env = gym.make(
