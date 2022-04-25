72596acf2d13785a48f8dcab92940d38fe30767d
Ben Glickenhaus <benglickenhaus@gmail.com>
2022-04-24 20:32:28 -0400

    train on all alphas

--- rl/scripts/test_dqn.py
+++ rl/scripts/test_dqn.py
@@ -26,19 +26,22 @@ config = dict(
     # ALPHA=0.35,
     ALGO="DQN",
     TOTAL_TIMESTEPS=10e6,
-    STEPS_PER_ROLLOUT=10,
+    STEPS_PER_ROLLOUT=250,
     LR_SCHEDULE=lr_schedule,
     BATCH_SIZE=64000,
     ALPHA_SCHEDULE_CUTOFF=0,
     LAYER_SIZE=100,
     N_LAYERS=2,
 )
-ALPHA = 0.25
+ALPHA = 0.35
 
 
 def env_fn(alpha):
     return gym.make(
-        "cpr-v0", spec=specs.nakamoto(alpha=alpha, n_steps=config["STEPS_PER_ROLLOUT"])
+        "cpr-v0",
+        spec=specs.nakamoto(
+            alpha=alpha, n_steps=config["STEPS_PER_ROLLOUT"], gamma=0, defenders=1
+        ),
     )
 
 
@@ -50,67 +53,39 @@ def alpha_schedule(step):
         alpha = max(alpha, 0.25)
     else:
         alpha = 0.25 * progress + 0.5 * (1 - progress)
+    return ALPHA
     return alpha
 
 
-env = gym.make("cpr-v0", spec=specs.nakamoto(alpha=ALPHA))
+env = env_fn(ALPHA)
 env = AlphaScheduleWrapper(env, env_fn, alpha_schedule)
-env = SparseRelativeRewardWrapper(env)
+env = SparseRelativeRewardWrapper(env, relative=False)
 
 obs = env.reset()
-while True:
-    action = env.action_space.sample()
-    obs, reward, done, info = env.step(action)
-    env.render()
-    if done:
-        obs = env.reset()
-        print(env.alpha)
+# while True:
+#     action = env.action_space.sample()
+#     obs, reward, done, info = env.step(action)
+#     env.render()
+#     if done:
+#         obs = env.reset()
+#         print(env.alpha)
 
-model = PPO.load(f"ppo_nakamoto_alpha_{ALPHA}")
-
-sum_rs = []
-sum_attacker_rs = []
-sum_defender_rs = []
-relative_rs = []
-actions = []
-_is = []
+model = PPO.load(f"saved_models/best_model.zip", env=env)
+test_rewards = []
 for _ in tqdm(range(100)):
     done = False
-    rs = []
-    attacker_rs = []
-    defender_rs = []
-    _actions = []
-    tds = []
-    i = 0
     obs = env.reset()
+    actions = []
     while not done:
-        action, _state = model.predict(np.array(obs), deterministic=True)
+        action, _state = model.predict(np.array(obs))
         next_obs, reward, done, info = env.step(action)
-        rs.append(reward)
-        attacker_rs.append(info["reward_attacker"])
-        defender_rs.append(info["reward_defender"])
-        tds.append(info["reward_time_elapsed"])
-        _actions.append(action)
-        i += 1
         obs = next_obs
-    _is.append(i)
-    actions += _actions
-    sum_rs.append(np.sum(rs))
-    sum_attacker_rs.append(np.sum(attacker_rs))
-    sum_defender_rs.append(np.sum(defender_rs))
-    relative_rs.append(
-        np.sum(attacker_rs) / (np.sum(defender_rs) + np.sum(attacker_rs))
-    )
-unique, unique_counts = np.unique(actions, return_counts=True)
-unique_counts = dict(zip(unique, unique_counts))
-print(f"Average reward: {np.mean(sum_rs)}, Std: {np.std(sum_rs)}")
-print(f"Average relative reward: {np.mean(relative_rs)}, Std: {np.std(relative_rs)}")
-print(f"Unique actions: {unique_counts}")
-print(f"Average steps: {np.mean(_is)}, Std: {np.std(_is)}")
-fig, ax = plt.subplots()
-sns.distplot(sum_rs, ax=ax)
-plt.show()
+        actions.append(action)
+    # print(actions)
+    test_rewards.append(reward)
+print(test_rewards)
 
+print(np.mean(test_rewards))
 # Average reward: 48464.505278942146, Std: 111065.6392517102
 
 """
