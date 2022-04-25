e5931182f331a27f6d869b4960ac2efabe9f9f02
Ben Glickenhaus <benglickenhaus@gmail.com>
2022-04-24 20:32:28 -0400

    train on all alphas

--- rl/scripts/train.py
+++ rl/scripts/train.py
@@ -29,6 +29,7 @@ from stable_baselines3.common.monitor import Monitor
 
 import wandb
 from wandb.sdk.lib import telemetry as wb_telemetry
+from wandb.integration.sb3 import WandbCallback
 
 
 class WandbCallback(BaseCallback):
@@ -95,7 +96,7 @@ class WandbCallback(BaseCallback):
             if len(x) > 0:
                 # Mean training reward over the last 100 episodes
                 mean_reward = np.mean(y[-1000:])
-                # wandb.log({"mean_reward": mean_reward})
+                wandb.log({"mean_reward": mean_reward})
 
                 # New best model, you could save the agent here
                 if mean_reward > self.best_mean_reward:
@@ -114,56 +115,6 @@ class WandbCallback(BaseCallback):
     def save_model(self) -> None:
         self.model.save(self.path)
         wandb.save(self.path, base_path=self.model_save_path)
-
-
-class SaveOnBestTrainingRewardCallback(BaseCallback):
-    """
-    Callback for saving a model (the check is done every ``check_freq`` steps)
-    based on the training reward (in practice, we recommend using ``EvalCallback``).
-
-    :param check_freq:
-    :param log_dir: Path to the folder where the model will be saved.
-      It must contains the file created by the ``Monitor`` wrapper.
-    :param verbose: Verbosity level.
-    """
-
-    def __init__(self, check_freq: int, log_dir: str, verbose: int = 1):
-        super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
-        self.check_freq = check_freq
-        self.log_dir = log_dir
-        self.save_path = os.path.join(log_dir, "best_model")
-        self.best_mean_reward = -np.inf
-
-    def _init_callback(self) -> None:
-        # Create folder if needed
-        if self.save_path is not None:
-            os.makedirs(self.save_path, exist_ok=True)
-
-    def _on_step(self) -> bool:
-        if self.n_calls % self.check_freq == 0:
-
-            # Retrieve training reward
-            x, y = ts2xy(load_results(self.log_dir), "timesteps")
-            if len(x) > 0:
-                # Mean training reward over the last 100 episodes
-                mean_reward = np.mean(y[-1000:])
-                # wandb.log({"mean_reward": mean_reward})
-
-                if self.verbose > 0:
-                    print(f"Num timesteps: {self.num_timesteps}")
-                    print(
-                        f"Best mean reward: {self.best_mean_reward:.2f} - Last mean reward per episode: {mean_reward:.2f}"
-                    )
-
-                # New best model, you could save the agent here
-                if mean_reward > self.best_mean_reward:
-                    self.best_mean_reward = mean_reward
-                    # Example for saving best model
-                    if self.verbose > 0:
-                        print(f"Saving new best model to {self.save_path}")
-                    self.model.save(self.save_path)
-
-        return True
 
 
 def lr_schedule(remaining):
@@ -213,7 +164,7 @@ def alpha_schedule(step):
         alpha = max(alpha, 0.05)
     else:
         alpha = 0.25 * progress + 0.5 * (1 - progress)
-    return 0.35
+    # return 0.35
     return alpha
 
 
