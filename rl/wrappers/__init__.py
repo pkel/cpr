5ea94e32007ff37e165f52dedbac64f3a9bbdb77
Ben Glickenhaus <benglickenhaus@gmail.com>
2022-05-04 09:48:12 -0400

    daa wrapper

--- rl/wrappers/__init__.py
+++ rl/wrappers/__init__.py
@@ -2,3 +2,4 @@ from .excess_reward_wrapper import RelativeRewardWrapper, SparseRelativeRewardWr
 from .exploration_reward_wrapper import ExplorationRewardWrapper
 from .illegal_move_wrapper import IllegalMoveWrapper
 from .honest_policy_wrapper import HonestPolicyWrapper
+from .daa_wrapper import DaaWrapper
--- rl/wrappers/decreasing_alpha_wrapper.py
+++ rl/wrappers/decreasing_alpha_wrapper.py
@@ -2,24 +2,51 @@ import re
 import gym
 import numpy as np
 from gym.spaces import Tuple, MultiDiscrete
+from typing import List
 
 
 class AlphaScheduleWrapper(gym.Wrapper):
-    def __init__(self, env, env_fn, alpha_schedule):
+    def __init__(self, env, env_fn, alpha_schedule: List, run_daa=False, target=600):
         super().__init__(env)
         self.alpha_schedule = alpha_schedule
         self.env_fn = env_fn
         self.current_step = 0
         self.alpha = None
+
+        # DAA
+        self.run_daa = run_daa
+        self.target = target
+        self.difficulties = dict((a, self.target) for a in alpha_schedule)
+        self.n_pow = 0
+        self.observed = 0
+
+    
+    def update_difficulties(self):
+        difficulty = self.target * self.target / self.observed
+        self.difficulties[self.alpha] = difficulty
+        self.n_pow = 0
+        self.observed = 0
         
 
     def reset(self):
         self.current_step += 1
-        alpha = self.alpha_schedule(self.current_step)
-        self.env = self.env_fn(alpha=alpha)
+        alpha = np.random.choice(self.alpha_schedule)
+
+        if self.run_daa:
+            self.update_difficulties()
+
+        self.env = self.env_fn(alpha=alpha, activation_delay=self.difficulties[alpha])
         self.alpha = alpha
+        
         obs = self.env.reset()
         
         return obs
+
+    def step(self, action):
+        obs, reward, done, info = self.env.step(action)
+        self.n_pow += info["reward_n_pows"]
+        if done:
+            self.observed = info['simulator_clock_rewarded'] / self.n_pow
+        return obs, reward, done, info
 
     
