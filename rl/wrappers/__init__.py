af77e135cd4458c3418b356909848a4831f1d05a
Ben Glickenhaus <benglickenhaus@gmail.com>
2022-05-04 10:10:53 -0400

    use absolute rewards with daa

--- rl/wrappers/__init__.py
+++ rl/wrappers/__init__.py
@@ -1,4 +1,9 @@
-from .excess_reward_wrapper import RelativeRewardWrapper, SparseRelativeRewardWrapper
+from .excess_reward_wrapper import (
+    RelativeRewardWrapper,
+    SparseRelativeRewardWrapper,
+    WastedBlocksRewardWrapper,
+    AbsoluteRewardWrapper,
+)
 from .exploration_reward_wrapper import ExplorationRewardWrapper
 from .illegal_move_wrapper import IllegalMoveWrapper
 from .honest_policy_wrapper import HonestPolicyWrapper
