ae872d717d37bbafa17a4e1b5523b55ff4c6e150
Patrik Keller <git@pkel.dev>
2022-05-20 13:51:20 +0200

    Parallel rollouts with SubprocVecEnv

--- python/gym/cpr_gym/envs.py
+++ python/gym/cpr_gym/envs.py
@@ -11,18 +11,22 @@ class Core(gym.Env):
         self.env = engine.create(proto, **kwargs)
         self.action_space = gym.spaces.Discrete(engine.n_actions(self.env))
         low = engine.observation_low(self.env)
+        low = np.array(low)  # for pickling; why doesn't pyml support pickling?
         high = engine.observation_high(self.env)
+        high = np.array(high)
         self.observation_space = gym.spaces.Box(low, high, dtype=np.float64)
         self.version = engine.cpr_lib_version
         self.policies = engine.policies(self.env)
 
     def reset(self):
-
-        return engine.reset(self.env)
+        obs = engine.reset(self.env)
+        obs = np.array(obs)  # for pickling; why doesn't pyml support pickling?
+        return obs
 
     def step(self, a):
-        s, r, d, i = engine.step(self.env, a)
-        return s, r, d, i
+        obs, r, d, i = engine.step(self.env, a)
+        obs = np.array(obs)  # for pickling; why doesn't pyml support pickling?
+        return obs, r, d, i
 
     def render(self, mode="ascii"):
         print(engine.to_string(self.env))
--- rl/scripts/train.py
+++ rl/scripts/train.py
@@ -1,17 +1,16 @@
-import sys, os
+import sys, os, psutil
 from uuid import uuid4
-
 
 sys.path.append(os.getcwd())
 import numpy as np
 
 import gym
 from cpr_gym import protocols
+import stable_baselines3
 from stable_baselines3 import A2C, PPO, DQN
 from stable_baselines3.ppo.policies import MlpPolicy
 from tqdm import tqdm
 import torch
-from stable_baselines3.common.env_util import make_vec_env
 from rl.wrappers.exploration_reward_wrapper import ExplorationRewardWrapper
 from rl.wrappers.excess_reward_wrapper import (
     RelativeRewardWrapper,
@@ -32,94 +31,97 @@ import wandb
 from wandb.sdk.lib import telemetry as wb_telemetry
 from wandb.integration.sb3 import WandbCallback
 
-
-class WandbCallback(BaseCallback):
-    """Log SB3 experiments to Weights and Biases
-        - Added model tracking and uploading
-        - Added complete hyperparameters recording
-        - Added gradient logging
-        - Note that `wandb.init(...)` must be called before the WandbCallback can be used
-
-    Args:
-        verbose: The verbosity of sb3 output
-        model_save_path: Path to the folder where the model will be saved, The default value is `None` so the model is not logged
-        model_save_freq: Frequency to save the model
-        gradient_save_freq: Frequency to log gradient. The default value is 0 so the gradients are not logged
-    """
-
-    def __init__(
-        self,
-        verbose: int = 0,
-        model_save_path: str = None,
-        model_save_freq: int = 0,
-        gradient_save_freq: int = 0,
-    ):
-        super(WandbCallback, self).__init__(verbose)
-        if wandb.run is None:
-            raise wandb.Error("You must call wandb.init() before WandbCallback()")
-        with wb_telemetry.context() as tel:
-            tel.feature.sb3 = True
-        self.model_save_freq = model_save_freq
-        self.model_save_path = model_save_path
-        self.gradient_save_freq = gradient_save_freq
-        self.best_mean_reward = -np.inf
-
-        # Create folder if needed
-        if self.model_save_path is not None:
-            os.makedirs(self.model_save_path, exist_ok=True)
-            self.path = os.path.join(self.model_save_path, "model.zip")
-        else:
-            assert (
-                self.model_save_freq == 0
-            ), "to use the `model_save_freq` you have to set the `model_save_path` parameter"
-
-    def _init_callback(self) -> None:
-        d = {}
-        if "algo" not in d:
-            d["algo"] = type(self.model).__name__
-        for key in self.model.__dict__:
-            if key in wandb.config:
-                continue
-            if type(self.model.__dict__[key]) in [float, int, str]:
-                d[key] = self.model.__dict__[key]
-            else:
-                d[key] = str(self.model.__dict__[key])
-        if self.gradient_save_freq > 0:
-            wandb.watch(self.model.policy, log_freq=self.gradient_save_freq, log="all")
-        wandb.config.setdefaults(d)
-
-    def _on_step(self) -> bool:
-        if self.model_save_freq > 0:
-            if self.model_save_path is not None:
-                if self.n_calls % self.model_save_freq == 0:
-                    difficulties = self.locals["infos"][0]["difficulties"]
-                    if difficulties is not None:
-                        for key, value in difficulties.items():
-                            wandb.log({f"difficulty/{key}": value})
-                    self.save_model()
-                    x, y = ts2xy(load_results(self.model_save_path), "timesteps")
-                    if len(x) > 0:
-                        # Mean training reward over the last 100 episodes
-                        mean_reward = np.mean(y[-5000:])
-                        wandb.log({"mean_reward": mean_reward})
-
-                        # New best model, you could save the agent here
-                        if mean_reward > self.best_mean_reward:
-                            self.best_mean_reward = mean_reward
-                            # Example for saving best model
-
-                            path = os.path.join(self.model_save_path, "best_model.zip")
-                            self.model.save(path)
-                            wandb.save(path, base_path=self.model_save_path)
-        return True
-
-    def _on_training_end(self) -> None:
-        if self.model_save_path is not None:
-            self.save_model()
-
-    def save_model(self) -> None:
-        self.model.save(self.path)
-        wandb.save(self.path, base_path=self.model_save_path)
+# TODO bglick13: This wrapper does not work with the parallel SubprocVecEnv.
+#                The imported one (wandb.integration.sb3) works. Maybe you
+#                can extend on that one?
+# class WandbCallback(BaseCallback):
+#     """Log SB3 experiments to Weights and Biases
+#         - Added model tracking and uploading
+#         - Added complete hyperparameters recording
+#         - Added gradient logging
+#         - Note that `wandb.init(...)` must be called before the WandbCallback can be used
+# 
+#     Args:
+#         verbose: The verbosity of sb3 output
+#         model_save_path: Path to the folder where the model will be saved, The default value is `None` so the model is not log
+# ged
+#         model_save_freq: Frequency to save the model
+#         gradient_save_freq: Frequency to log gradient. The default value is 0 so the gradients are not logged
+#     """
+# 
+#     def __init__(
+#         self,
+#         verbose: int = 0,
+#         model_save_path: str = None,
+#         model_save_freq: int = 0,
+#         gradient_save_freq: int = 0,
+#     ):
+#         super(WandbCallback, self).__init__(verbose)
+#         if wandb.run is None:
+#             raise wandb.Error("You must call wandb.init() before WandbCallback()")
+#         with wb_telemetry.context() as tel:
+#             tel.feature.sb3 = True
+#         self.model_save_freq = model_save_freq
+#         self.model_save_path = model_save_path
+#         self.gradient_save_freq = gradient_save_freq
+#         self.best_mean_reward = -np.inf
+# 
+#         # Create folder if needed
+#         if self.model_save_path is not None:
+#             os.makedirs(self.model_save_path, exist_ok=True)
+#             self.path = os.path.join(self.model_save_path, "model.zip")
+#         else:
+#             assert (
+#                 self.model_save_freq == 0
+#             ), "to use the `model_save_freq` you have to set the `model_save_path` parameter"
+# 
+#     def _init_callback(self) -> None:
+#         d = {}
+#         if "algo" not in d:
+#             d["algo"] = type(self.model).__name__
+#         for key in self.model.__dict__:
+#             if key in wandb.config:
+#                 continue
+#             if type(self.model.__dict__[key]) in [float, int, str]:
+#                 d[key] = self.model.__dict__[key]
+#             else:
+#                 d[key] = str(self.model.__dict__[key])
+#         if self.gradient_save_freq > 0:
+#             wandb.watch(self.model.policy, log_freq=self.gradient_save_freq, log="all")
+#         wandb.config.setdefaults(d)
+# 
+#     def _on_step(self) -> bool:
+#         if self.model_save_freq > 0:
+#             if self.model_save_path is not None:
+#                 if self.n_calls % self.model_save_freq == 0:
+#                     difficulties = self.locals["infos"][0]["difficulties"]
+#                     if difficulties is not None:
+#                         for key, value in difficulties.items():
+#                             wandb.log({f"difficulty/{key}": value})
+#                     self.save_model()
+#                     x, y = ts2xy(load_results(self.model_save_path), "timesteps")
+#                     if len(x) > 0:
+#                         # Mean training reward over the last 100 episodes
+#                         mean_reward = np.mean(y[-5000:])
+#                         wandb.log({"mean_reward": mean_reward})
+# 
+#                         # New best model, you could save the agent here
+#                         if mean_reward > self.best_mean_reward:
+#                             self.best_mean_reward = mean_reward
+#                             # Example for saving best model
+# 
+#                             path = os.path.join(self.model_save_path, "best_model.zip")
+#                             self.model.save(path)
+#                             wandb.save(path, base_path=self.model_save_path)
+#         return True
+# 
+#     def _on_training_end(self) -> None:
+#         if self.model_save_path is not None:
+#             self.save_model()
+# 
+#     def save_model(self) -> None:
+#         self.model.save(self.path)
+#         wandb.save(self.path, base_path=self.model_save_path)
 
 
 def lr_schedule(remaining):
@@ -187,70 +189,76 @@ config = dict(
     ACTIVATION_DELAY=1,
 )
 
+def single_env_fn():
+    env = env_fn(0, 1, config)
+    env = AlphaScheduleWrapper(env, env_fn, config)
+    if config["USE_DAA"]:
+        env = AbsoluteRewardWrapper(env)
+    else:
+        env = WastedBlocksRewardWrapper(env)
+    env = Monitor(env, log_dir)
+    return env
 
-wandb.init(project="dqn", entity="tailstorm", config=config)
-# config = wandb.config
+if __name__ == "__main__":
+    wandb.init(project="dqn", entity="tailstorm", config=config)
+    # config = wandb.config
 
-log_dir = f"saved_models/{wandb.run.id}"
-# log_dir = f"saved_models/test"
+    log_dir = f"saved_models/{wandb.run.id}"
+    # log_dir = f"saved_models/test"
 
-os.makedirs(log_dir, exist_ok=True)
-env = env_fn(0, 1, config)
-env = AlphaScheduleWrapper(env, env_fn, config)
-if config["USE_DAA"]:
-    env = AbsoluteRewardWrapper(env)
-else:
-    env = WastedBlocksRewardWrapper(env)
+    os.makedirs(log_dir, exist_ok=True)
 
-env = Monitor(env, log_dir)
-print(env.action_space)
-if config["ALGO"] == "PPO":
-    policy_kwargs = dict(
-        activation_fn=torch.nn.ReLU,
-        net_arch=[
-            dict(
-                pi=[config["LAYER_SIZE"]] * config["N_LAYERS"],
-                vf=[config["LAYER_SIZE"]] * config["N_LAYERS"],
-            )
-        ],
+    n_envs = psutil.cpu_count(logical=False) # count physical cores; hyper-threading does not help us here.
+    env = stable_baselines3.common.vec_env.SubprocVecEnv([single_env_fn] * n_envs)
+    print(env.action_space)
+    if config["ALGO"] == "PPO":
+        policy_kwargs = dict(
+            activation_fn=torch.nn.ReLU,
+            net_arch=[
+                dict(
+                    pi=[config["LAYER_SIZE"]] * config["N_LAYERS"],
+                    vf=[config["LAYER_SIZE"]] * config["N_LAYERS"],
+                )
+            ],
+        )
+        model = PPO(
+            "MlpPolicy",
+            env,
+            verbose=1,
+            batch_size=config["BATCH_SIZE"],
+            n_steps=config["BATCH_SIZE"] * config["N_STEPS_MULTIPLE"],
+            clip_range=0.1,
+            # ent_coef=0.01,
+            learning_rate=lr_schedule,
+            # clip_range=clip_schedule,
+            policy_kwargs=policy_kwargs,
+        )
+    elif config["ALGO"] == "DQN":
+        policy_kwargs = dict(
+            activation_fn=torch.nn.ReLU,
+            net_arch=[config["LAYER_SIZE"]] * config["N_LAYERS"],
+        )
+        model = DQN(
+            "MlpPolicy",
+            env,
+            verbose=1,
+            batch_size=config["BATCH_SIZE"],
+            # clip_range=0.1,
+            # ent_coef=0.01,
+            learning_rate=lr_schedule,
+            # clip_range=clip_schedule,
+            policy_kwargs=policy_kwargs,
+            exploration_initial_eps=config["STARTING_EPS"],
+            exploration_final_eps=config["ENDING_EPS"],
+        )
+
+    model.learn(
+        total_timesteps=config["TOTAL_TIMESTEPS"],
+        callback=WandbCallback(
+            gradient_save_freq=1000,
+            model_save_path=log_dir,
+            model_save_freq=1000,
+            verbose=0,
+        ),
     )
-    model = PPO(
-        "MlpPolicy",
-        env,
-        verbose=1,
-        batch_size=config["BATCH_SIZE"],
-        n_steps=config["BATCH_SIZE"] * config["N_STEPS_MULTIPLE"],
-        clip_range=0.1,
-        # ent_coef=0.01,
-        learning_rate=lr_schedule,
-        # clip_range=clip_schedule,
-        policy_kwargs=policy_kwargs,
-    )
-elif config["ALGO"] == "DQN":
-    policy_kwargs = dict(
-        activation_fn=torch.nn.ReLU,
-        net_arch=[config["LAYER_SIZE"]] * config["N_LAYERS"],
-    )
-    model = DQN(
-        "MlpPolicy",
-        env,
-        verbose=1,
-        batch_size=config["BATCH_SIZE"],
-        # clip_range=0.1,
-        # ent_coef=0.01,
-        learning_rate=lr_schedule,
-        # clip_range=clip_schedule,
-        policy_kwargs=policy_kwargs,
-        exploration_initial_eps=config["STARTING_EPS"],
-        exploration_final_eps=config["ENDING_EPS"],
-    )
-model.learn(
-    total_timesteps=config["TOTAL_TIMESTEPS"],
-    callback=WandbCallback(
-        gradient_save_freq=1000,
-        model_save_path=log_dir,
-        model_save_freq=1000,
-        verbose=0,
-    ),
-)
-model.save(os.path.join(log_dir, f"{config['ALGO']}_bkll"))
+    model.save(os.path.join(log_dir, f"{config['ALGO']}_bkll"))
