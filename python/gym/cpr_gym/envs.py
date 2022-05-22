0caebb47f1a642470791665513a11119240607ce
Patrik Keller <git@pkel.dev>
2022-05-22 22:25:20 +0200

    update alpha range

--- python/gym/cpr_gym/envs.py
+++ python/gym/cpr_gym/envs.py
@@ -129,7 +129,7 @@ class Wip(Core):
 
     def reset(self):
         # sample alpha
-        self.alpha = random.uniform(0.6, 0.9)
+        self.alpha = random.uniform(0.4, 0.6)
         # estimate difficulty
         i = np.nonzero(
             np.abs(self.rb_alpha.buf - self.alpha) < 0.025
