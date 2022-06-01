49147f97e1408d6c58804ce044a3aaab86665e6a
Patrik Keller <git@pkel.dev>
2022-06-01 18:51:53 +0200

    Add SparseRewardPerBlockWrapper to cpr_gym.
    Also do some testing in an ipynb.

--- python/gym/cpr_gym/wrappers.py
+++ python/gym/cpr_gym/wrappers.py
@@ -30,3 +30,38 @@ class SparseRelativeRewardWrapper(gym.Wrapper):
         else:
             reward = 0
         return obs, reward, done, info
+
+
+class SparseRewardPerBlockWrapper(gym.Wrapper):
+    """
+    Overwrites objective function.
+    Calculates reward per confirmed proof-of-work puzzle solution at the end of episode.
+    One reward per episode.
+
+    This is similar to the SparseRelativeRewardWrapper. The two wrappers are
+    equivalent for protocols or reward functions that assign a constant reward
+    per confirmed puzzle solution. For different reward functions, e.g.
+    Tailstorm's 'discount' scheme, this wrapper is better suited.
+    """
+
+    def __init__(self, env):
+        super().__init__(env)
+
+    def reset(self):
+        obs = self.env.reset()
+        self.srpbw_acc = 0
+        self.srpbw_pow = 0
+        return obs
+
+    def step(self, action):
+        obs, _reward, done, info = self.env.step(action)
+        self.srpbw_acc += info["reward_attacker"]
+        self.srpbw_pow += info["reward_n_pows"]
+        if done:
+            if self.srpbw_pow != 0:
+                reward = self.srpbw_acc / self.srpbw_pow
+            else:
+                reward = 0
+        else:
+            reward = 0
+        return obs, reward, done, info
