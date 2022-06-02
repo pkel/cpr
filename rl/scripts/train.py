bd74a46c2e07ea6a5a6b48e9d5a391243a5e2506
Patrik Keller <git@pkel.dev>
2022-06-02 12:20:15 +0200

    Push sb3 logs to WandB

--- rl/scripts/train.py
+++ rl/scripts/train.py
@@ -114,6 +114,15 @@ config = dict(
 )
 
 
+class WandbKVWriter(stable_baselines3.common.logger.KVWriter):
+    def write(self, key_values, key_excluded, step=0):
+        wandb.log(data=key_values, step=step)
+        return
+
+    def close(self):
+        return
+
+
 class LogDaaBufferCallback(BaseCallback):
     def __init__(self, prefix="daa_buffer"):
         super().__init__()
@@ -311,6 +320,10 @@ if __name__ == "__main__":
             exploration_initial_eps=config["STARTING_EPS"],
             exploration_final_eps=config["ENDING_EPS"],
         )
+
+    logger = stable_baselines3.common.utils.configure_logger(verbose=1)
+    logger.output_formats.append(WandbKVWriter())
+    model.set_logger(logger)
 
     model.learn(
         total_timesteps=config["TOTAL_TIMESTEPS"],
