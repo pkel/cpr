9b34f2311a0049845493c14157f039875866cea2
Ben Glickenhaus <benglickenhaus@gmail.com>
2022-06-02 14:48:50 -0400

    release on done wrapper

--- rl/wrappers/release_on_done_wrapper.py
+++ rl/wrappers/release_on_done_wrapper.py
@@ -13,18 +13,18 @@ class ReleaseOnDoneWrapper(gym.Wrapper):
         if done:
             public_blocks = obs[0]
             private_blocks = obs[1]
-            info["reward_n_pows"] = max(public_blocks, private_blocks)
+            info["reward_n_pows"] += max(public_blocks, private_blocks)
             if public_blocks > private_blocks:
-                info["reward_defender"] = public_blocks
+                info["reward_defender"] += public_blocks
             elif private_blocks > public_blocks:
-                info["reward_attacker"] = private_blocks
+                info["reward_attacker"] += private_blocks
             elif public_blocks == private_blocks:
                 private_wins = np.random.choice(
                     [True, False], p=[self.env.alpha, 1 - self.env.alpha]
                 )
                 if private_wins:
-                    info["reward_attacker"] = private_blocks
+                    info["reward_attacker"] += private_blocks
                 else:
-                    info["reward_defender"] = public_blocks
+                    info["reward_defender"] += public_blocks
 
         return obs, reward, done, info
