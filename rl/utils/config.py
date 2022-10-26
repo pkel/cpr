d238a5df09b39b44bef12bb650efb726da72f459
Patrik Keller <git@pkel.dev>
2022-10-26 16:07:13 +0200

    Patrik trains on alpha 1/3

--- rl/utils/config.py
+++ rl/utils/config.py
@@ -21,15 +21,15 @@ class Config(BaseSettings):
     STARTING_EPS: float = 0.99
     ENDING_EPS: float = 0.01
     ALPHA_SCHEDULE: List[float] = [
-        0.15,
-        0.25,
+        #  0.15,
+        #  0.25,
         1 / 3.0,
-        0.35,
-        0.375,
-        0.4,
-        0.425,
-        0.45,
-        0.475,
+        #  0.35,
+        #  0.375,
+        #  0.4,
+        #  0.425,
+        #  0.45,
+        #  0.475,
     ]
     REWARD_WRAPPER: Literal[
         "SparseDaaRewardWrapper",
