bd2b8057abec4f9f2ceb14e18215bb2c4f45f933
Patrik Keller <git@pkel.dev>
2022-11-15 21:53:54 +0100

    Github workflow and python update script

--- python/gym/cpr_gym/__init__.py
+++ python/gym/cpr_gym/__init__.py
@@ -11,34 +11,25 @@ try:
     curdir = dir_path = os.path.dirname(os.path.realpath(__file__))
     dll_basename = "bridge.so"
     dll_name = f"{curdir}/{dll_basename}"
-    if sys.platform == "linux":
-        dll_url = "https://pkel.github.io/cpr/linux/bridge.so"
-    elif sys.platform == "darwin":
-        dll_url = "https://pkel.github.io/cpr/macos/bridge.so"
-    else:
-        raise OSError(f"Unsupported OS: '{sys.platform}' - we support Linux and MacOS")
+
+    # Load DLL
     dll = PyDLL(dll_name, RTLD_GLOBAL)
     argv_t = c_char_p * 2
     argv = argv_t(dll_basename.encode("utf-8"), None)
     dll.caml_startup(argv)
+
+    # Register gym environments
+    gym.envs.register(id="core-v0", entry_point="cpr_gym.envs:Core")
+
+    # Make dll-dependent modules available
+    import engine, protocols  # noqa
 except OSError:
-    if not os.path.exists(dll_name):
-        print(
-            "\n"
-            f"{dll_name} not found, please download {dll_basename} "
-            f"from {dll_url} and place into {curdir}:\n\n"
-            f"curl {dll_url} -o {dll_name}\n\n"
-            "you can later update the library with\n\n"
-            f"{sys.executable} -m {__name__} --update\n"
-        )
-        raise ImportError(
-            f"{dll_name} not found, please download {dll_basename} "
-            f"from {dll_url} and place into {curdir}"
-        )
-    raise
-
-import engine, protocols  # noqa
-
-#  Register gym environments
-
-gym.envs.register(id="core-v0", entry_point="cpr_gym.envs:Core")
+    if os.path.exists(dll_name):
+        raise
+    print(
+        f"WARNING: missing DLL "
+        f"{os.path.relpath(dll_name)}. "
+        "The program is likely to fail. Run "
+        f"`{os.path.relpath(sys.executable)} -m cpr_gym --update` "
+        "to get the latest DLL from Github."
+    )
--- python/gym/cpr_gym/__main__.py
+++ python/gym/cpr_gym/__main__.py
@@ -1,8 +1,9 @@
 import argparse
 import cpr_gym
-import urllib.request
+import os
+import requests
 import shutil
-
+import sys
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -12,11 +13,27 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     if args.update:
-        print(f"downloading bridge.so to {cpr_gym.dll_name}_new")
-        urllib.request.urlretrieve(cpr_gym.dll_url, cpr_gym.dll_name + "_new")
-        print(f"moving {cpr_gym.dll_name} -> {cpr_gym.dll_name}_old")
-        shutil.move(cpr_gym.dll_name, cpr_gym.dll_name + "_old")
-        print(f"moving {cpr_gym.dll_name}_new -> {cpr_gym.dll_name}")
-        shutil.move(cpr_gym.dll_name + "_new", cpr_gym.dll_name)
+        rel = requests.get(
+            "https://api.github.com/repos/pkel/cpr/releases/latest"
+        ).json()
+        tag = rel["tag_name"]
+        if sys.platform not in ["linux", "darwin"]:
+            raise OSError(
+                f"Unsupported OS: '{sys.platform}' - we support Linux and MacOS"
+            )
+        repo_url = "https://github.com/pkel/cpr"
+        dll_url = f"{repo_url}/releases/download/{tag}/cpr-{tag}-{sys.platform}.so"
+
+        if os.path.exists(cpr_gym.dll_name):
+            print(f"backup bridge to {os.path.relpath(cpr_gym.dll_name)}_old")
+            shutil.move(cpr_gym.dll_name, cpr_gym.dll_name + "_old")
+
+        print(f"download new bridge from {dll_url}")
+        so = requests.get(dll_url, stream=True)
+        so.raise_for_status()
+        with open(f"{cpr_gym.dll_name}", "wb") as handle:
+            for block in so.iter_content(1024):
+                handle.write(block)
+
     if args.version:
         print(cpr_gym.engine.cpr_lib_version)
