9894fd5f5c9ba9e638078b49e3a0eae72c32d956
Patrik Keller <git@pkel.dev>
2022-12-07 14:34:05 +0100

    Build less wheels

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import shutil
 import subprocess
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
+from wheel.bdist_wheel import bdist_wheel
 
 with open("VERSION", "r") as f:
     version = f.read().strip()
@@ -16,6 +17,15 @@ try:
         version = f"{version}+dev"
 except subprocess.CalledProcessError:
     full_version = version
+
+
+class my_bdist_wheel(bdist_wheel):
+    def get_tag(self):
+        python, abi, plat = super().get_tag()
+        # apparently, pyml DLL's are compatible with Python 2 & 3
+        # see https://github.com/thierry-martinez/pyml
+        # so I guess it's save to set abi = none here
+        return "py", "none", plat
 
 
 class my_build_ext(build_ext):
@@ -50,6 +60,6 @@ setup(
     packages=["cpr_gym"],
     package_dir={"cpr_gym": "./gym/cpr_gym"},
     ext_modules=[Extension(name="bridge", sources=["simulator/gym/bridge.ml"])],
-    cmdclass=dict(build_ext=my_build_ext),
+    cmdclass=dict(bdist_wheel=my_bdist_wheel, build_ext=my_build_ext),
     install_requires=["gym", "numpy"],
 )
