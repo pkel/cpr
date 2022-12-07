f1f3a42edb946b45ba83a9e1f25739b1376176dd
Patrik Keller <git@pkel.dev>
2022-12-07 13:34:24 +0100

    --version command doc

--- gym/cpr_gym/__main__.py
+++ gym/cpr_gym/__main__.py
@@ -7,9 +7,7 @@ import sys
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--version", action="store_true", help="print OCaml DLL version"
-    )
+    parser.add_argument("--version", action="store_true", help="print version")
     args = parser.parse_args()
 
     if args.version:
