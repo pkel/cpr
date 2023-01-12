15bdaa605721b8d1ebd3e15522306b74b49ef680
Patrik Keller <git@pkel.dev>
2023-01-12 20:52:47 +0100

    Distributed training. Parallelize runs on individual hosts

--- experiments/train/parallel.sh
+++ experiments/train/parallel.sh
@@ -4,8 +4,9 @@ set -Eeuo pipefail
 
 branch=origin/training
 
-alphas=(33 40 45)
+alphas=(20 25 30 35 40 45)
 gammas=(05 50 95)
+repeat=(1 2 3) # how often should each config be repeated?
 protos=(
   nakamoto
   # bk-8
@@ -13,14 +14,30 @@ protos=(
 )
 
 hosts=(
-  # 1/localhost
-  1/athene
-  1/iris
-  1/nike
+  8/localhost
+  4/athene
+  4/iris
+  4/nike
 )
 servers=$(printf ",%s" "${hosts[@]}")
 servers=${servers:1}
 
+# run once per host
+setup () (
+  set -Eeuo pipefail
+  set -x
+  {
+    hostname
+
+    git fetch
+    git checkout "$branch"
+
+    make python=python3.10 _venv build
+
+  } 2>&1
+)
+
+# might run in parallel on individual host
 ppo () (
   proto=$1
   alpha=$2
@@ -30,26 +47,20 @@ ppo () (
   set -x
 
   {
-    git fetch
-    git checkout "$branch"
-
-    make python=python3.10 _venv build
-
-    # TODO it might be useful to run the above commands once per batch in
-    # advance. Then version is used of batch start time, not job start time.
-
     # shellcheck disable=SC1091
     . _venv/bin/activate
     which python
     python --version
 
     cd experiments/train
-    python ppo.py "$proto" --alpha "$alpha" --gamma "$gamma" --batch --tag "$name"
+    buf=$(mktemp ppo-XXXXXXXXX-out)
+    python ppo.py "$proto" --alpha "$alpha" --gamma "$gamma" --batch --tag "$name" | tee "$buf"
 
-    cd saved_models
-    # shellcheck disable=SC2012
-    ret=$(ls -td ./*/ | head -n 1)
-    zip ../../../"ppo-$proto-alpha$alpha-gamma$gamma.zip" -r "$ret"
+    # locate and zip output directory
+    out=$(grep -o "saved_models/ppo-$proto-alpha$alpha-gamma$gamma-[A-Za-z0-9-]*" "$buf")
+    rm "$buf"
+    zip ../../../"ppo-$proto-alpha$alpha-gamma$gamma.zip" -r "$out"
+
   } 2>&1
 )
 
@@ -61,19 +72,29 @@ fi
 mkdir "$name.results"
 cd "$name.results"
 
-export -f ppo
+export -f ppo setup
 export branch name
 
+parallel -S "$servers" --nonall \
+  --env setup --env branch --env name \
+  --workdir cpr \
+  --results setup \
+  --joblog "job.log" \
+  --eta \
+  setup
+
 parallel -S "$servers" \
-  --env ppo --env branch --env name \
+  --controlmaster --sshdelay 0.1 \
+  --env ppo --env name \
   --workdir cpr \
   --return "ppo-{proto}-alpha{alpha}-gamma{gamma}.zip" \
   --cleanup \
   --results "./ppo-{proto}-alpha{alpha}-gamma{gamma}" \
-  --joblog "job.log" \
-  --progress \
+  --joblog "+job.log" \
+  --eta \
   --header : \
   ppo \
   ::: proto "${protos[@]}" \
   ::: alpha "${alphas[@]}" \
-  ::: gamma "${gammas[@]}"
+  ::: gamma "${gammas[@]}" \
+  ::: dummy "${repeat[@]}"
