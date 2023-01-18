45cdee539c77f2885fa252344d9ee19d6aea9f44
Patrik Keller <git@pkel.dev>
2023-01-18 16:16:30 +0100

    Improve distributed training

--- experiments/train/parallel.sh
+++ experiments/train/parallel.sh
@@ -4,21 +4,26 @@ set -Eeuo pipefail
 
 branch=origin/training
 
-alphas=(20 25 30 35 40 45)
-gammas=(05 50 95)
-iteris=(1) # how often should each config be repeated?
-shapes=(raw cut exp)
 protos=(
   nakamoto
-  bk-8
-  tailstorm-8-constant
+  # bk-8
+  # tailstorm-8-constant
 )
+alphas=(20 25 30 35 40 45)
+alphas=(40)
+gammas=(05 50 95)
+gammas=(05)
+shapes=(raw cut exp)
+iteris=(1 2) # how often should each config be repeated?
 
 hosts=(
-  # 8/localhost
-  4/athene
-  4/iris
+  # 6/localhost
+  # 4/athene
+  # 4/iris
   # 4/nike
+  2/athene
+  2/iris
+  2/nike
 )
 servers=$(printf ",%s" "${hosts[@]}")
 servers=${servers:1}
@@ -40,13 +45,10 @@ setup () (
 
 # might run in parallel on individual host
 ppo () (
-  iteri=$1
-  shape=$2
-  proto=$3
-  alpha=$4
-  gamma=$5
+  jobnr=$1
+  shift
+
   set -Eeuo pipefail
-  set -x
 
   {
     # shellcheck disable=SC1091
@@ -54,14 +56,15 @@ ppo () (
     which python
     python --version
 
+    set -x
     cd experiments/train
     buf=$(mktemp ppo-XXXXXXXXX-out)
-    python ppo.py "$proto" --alpha "$alpha" --gamma "$gamma" --shape "$shape" --batch --tag "$name" | tee "$buf"
+    echo python ppo.py --batch --tag "$name" "${@}" | tee "$buf"
 
-    # locate and zip output directory
-    out=$(grep -o "saved_models/ppo-$proto-alpha$alpha-gamma$gamma-[A-Za-z0-9-]*" "$buf")
+    locate and zip output directory
+    out=$(grep -o "saved_models/ppo-[A-Za-z0-9-]*" "$buf")
     rm "$buf"
-    zip ../../"ppo-$proto-alpha$alpha-gamma$gamma-$iteri.zip" -r "$out"
+    zip ../../"ppo-$name-$jobnr.zip" -r "$out"
 
   } 2>&1
 )
@@ -81,7 +84,7 @@ parallel -S "$servers" --nonall \
   --env setup --env branch --env name \
   --workdir cpr \
   --results setup \
-  --joblog "job.log" \
+  --joblog "setup.job.log" \
   --eta \
   setup
 
@@ -89,15 +92,16 @@ parallel -S "$servers" \
   --controlmaster --sshdelay 0.1 \
   --env ppo --env name \
   --workdir cpr \
-  --return "ppo-{proto}-alpha{alpha}-gamma{gamma}-{shape}-{iteri}.zip" \
+  --return "ppo-$name-{#}.zip" \
   --cleanup \
-  --results "./ppo-{proto}-alpha{alpha}-gamma{gamma}-{shape}-{iteri}" \
-  --joblog "+job.log" \
+  --results "./ppo-$name-{#}" \
+  --joblog "ppo.job.log" \
+  --retries 2 \
   --eta \
   --header : \
-  ppo \
-  ::: iteri "${iteris[@]}" \
-  ::: shape "${shapes[@]}" \
+  ppo "{#}" "{proto}" --alpha "{alpha}" --gamma "{gamma}" --shape "{shape}" \
   ::: proto "${protos[@]}" \
   ::: alpha "${alphas[@]}" \
-  ::: gamma "${gammas[@]}"
+  ::: gamma "${gammas[@]}" \
+  ::: shape "${shapes[@]}" \
+  ::: iteri "${iteris[@]}"
