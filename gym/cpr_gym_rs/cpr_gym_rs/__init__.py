16dedd8ee534674577a1bce01ae1a2ef46fae06e
Patrik Keller <git@pkel.dev>
2023-12-26 14:15:44 +0100

    First training run with stable-baselines3 zoo

--- gym/cpr_gym_rs/cpr_gym_rs/__init__.py
+++ gym/cpr_gym_rs/cpr_gym_rs/__init__.py
@@ -1,8 +1,13 @@
 from gymnasium.envs.registration import register
 
 register(
-    id="cpr_gym_rs/FC16SSZwPT-v0",
+    id="FC16SSZwPT-v0",
     entry_point="cpr_gym_rs.envs:FC16SSZwPT",
     nondeterministic=True,
     order_enforce=False,
+    kwargs=dict(
+        alpha=0.4,
+        gamma=0.0,
+        horizon=25,
+    ),
 )
