941e6bda41bdd96184995543d3d718a948bbb95b
Patrik Keller <git@pkel.dev>
2023-12-26 14:15:44 +0100

    First training run with stable-baselines3 zoo

--- gym/cpr_gym_rs/test/test_regenvs.py
+++ gym/cpr_gym_rs/test/test_regenvs.py
@@ -1,9 +1,8 @@
+import cpr_gym_rs  # noqa: 401
 import gymnasium.utils.env_checker
 
 
 def test_FC16SSZwPT():
     for _ in range(10):
-        env = gymnasium.make(
-            "cpr_gym_rs/FC16SSZwPT-v0", alpha=0.25, gamma=0.25, horizon=5
-        )
+        env = gymnasium.make("FC16SSZwPT-v0", alpha=0.25, gamma=0.25, horizon=5)
         gymnasium.utils.env_checker.check_env(env.unwrapped, skip_render_check=True)
