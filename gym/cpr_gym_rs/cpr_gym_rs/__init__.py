358b6517d09f6f7c22922bd6bc1afae0febaad89
Patrik Keller <git@pkel.dev>
2023-12-27 15:48:53 +0100

    Force termination

--- gym/cpr_gym_rs/cpr_gym_rs/__init__.py
+++ gym/cpr_gym_rs/cpr_gym_rs/__init__.py
@@ -5,9 +5,10 @@ register(
     entry_point="cpr_gym_rs.envs:FC16SSZwPT",
     nondeterministic=True,
     order_enforce=False,
+    max_episode_steps=1000,  # forced termination, not Markovian
     kwargs=dict(
-        alpha=0.4,
-        gamma=0.0,
-        horizon=25,
+        alpha=1 / 3,
+        gamma=0.5,
+        horizon=100,
     ),
 )
