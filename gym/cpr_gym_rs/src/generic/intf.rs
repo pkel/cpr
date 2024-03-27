63ea35dd614870e85ec5e78339c56af6924a0756
Patrik Keller <git@pkel.dev>
2024-03-27 16:04:59 +0100

    Let protocols store generic data on the block

--- gym/cpr_gym_rs/src/generic/intf.rs
+++ gym/cpr_gym_rs/src/generic/intf.rs
@@ -1,6 +1,6 @@
 // Protocol Specification Interface
 
-pub trait BlockDAG {
+pub trait BlockDAG<Data> {
     type Block: Copy;
     type Miner: Copy;
     type BlockIter: Iterator<Item = Self::Block>;
@@ -20,39 +20,47 @@ pub trait BlockDAG {
     // Who mined block `b`?
     // This will be used to allocate rewards.
 
-    fn height(&self, b: Self::Block) -> u32;
-    // TODO, move this into crate::proto::nakamoto
+    fn data(&self, b: Self::Block) -> &Data;
+    // Extract protocol-dependent block data.
 }
 
 pub trait Protocol {
-    type BlockIter<D: BlockDAG> : Iterator<Item = D::Block>;
+    type Data; // protocol-dependent block data
+    type BlockIter<D: BlockDAG<Self::Data>>: Iterator<Item = D::Block>;
+
+    // genesis block
+    fn init(&self) -> Self::Data;
 
     // honest behaviour: mining
-    fn mining<D: BlockDAG>(&self, d: D, ep: D::Block) -> Self::BlockIter<D>;
+    fn mining<D: BlockDAG<Self::Data>>(
+        &self,
+        d: D,
+        ep: D::Block,
+    ) -> (Self::BlockIter<D>, Self::Data);
     // Given entrypoint `ep`, how do honest nodes extend the chain?
     // The returned iterator feeds into the parents of the newly mined block.
 
     // // honest behaviour: preference update
-    fn update<D: BlockDAG>(&self, d: D, ep: D::Block, b: D::Block) -> D::Block;
+    fn update<D: BlockDAG<Self::Data>>(&self, d: D, ep: D::Block, b: D::Block) -> D::Block;
     // // Given entrypoint `ep` and novel block `b`, how do honest nodes update their entrypoint?
 
     // // linear history: tip of chain
-    fn tip<D: BlockDAG>(&self, d: D, ep: D::Block) -> D::Block;
+    fn tip<D: BlockDAG<Self::Data>>(&self, d: D, ep: D::Block) -> D::Block;
     // // Given entrypoint `ep`, what is considered the latest block in the linear chain of blocks?
 
     // // linear history: walking backwards
-    fn pred<D: BlockDAG>(&self, d: D, b: D::Block) -> D::Block;
+    fn pred<D: BlockDAG<Self::Data>>(&self, d: D, b: D::Block) -> Option<D::Block>;
     // // Given block `b`, what's the parent of `b` in the linear chain of blocks?
 
     // // difficulty adjustment
-    fn progress<D: BlockDAG>(&self, d: D, b: D::Block) -> f32;
+    fn progress<D: BlockDAG<Self::Data>>(&self, d: D, b: D::Block) -> f32;
     // // Given block `b`, what's the mining contribution since `pred(b)`?
     // // DAA's goal tries to maintain a constant mining contribution rate per time.
     // // Block `b` is guaranteed to be part of a linear history of some tip.
 
     // // incentive scheme
-    type RewardAlloc<D: BlockDAG>: Iterator<Item = (D::Miner, f32)>;
-    fn reward<D: BlockDAG>(&self, d: D, b: D::Block) -> Self::RewardAlloc<D>;
+    type RewardAlloc<D: BlockDAG<Self::Data>>: Iterator<Item = (D::Miner, f32)>;
+    fn reward<D: BlockDAG<Self::Data>>(&self, d: D, b: D::Block) -> Self::RewardAlloc<D>;
     // // Given block `b`, what rewards where allocated since `pred(b)`?
     // // Block `b` is guaranteed to be part of a linear history of some tip.
 }
--- gym/cpr_gym_rs/src/proto/nakamoto.rs
+++ gym/cpr_gym_rs/src/proto/nakamoto.rs
@@ -1,39 +1,52 @@
-struct Nakamoto {}
-
 use crate::generic::intf::{BlockDAG, Protocol};
 
-use std::iter::{Once, once};
+struct Nakamoto {}
+
+struct Data {
+    height: u32,
+}
+
+use std::iter::{once, Once};
 
 impl Protocol for Nakamoto {
-    type BlockIter<D:BlockDAG> = Once<D::Block>;
+    type Data = Data;
+    type BlockIter<D: BlockDAG<Data>> = Once<D::Block>;
 
-    fn mining<D: BlockDAG>(&self, d: D, ep: D::Block) -> Self::BlockIter<D> {
-        once(ep)
+    fn init(&self) -> Data {
+        Data { height: 0 }
     }
 
-    fn update<D: BlockDAG>(&self, d: D, ep: D::Block, b: D::Block) -> D::Block {
-        if d.height(b) > d.height(ep) {
+    fn mining<D: BlockDAG<Data>>(&self, d: D, ep: D::Block) -> (Self::BlockIter<D>, Data) {
+        let parents = once(ep);
+        let data = Data {
+            height: d.data(ep).height + 1,
+        };
+        (parents, data)
+    }
+
+    fn update<D: BlockDAG<Data>>(&self, d: D, ep: D::Block, b: D::Block) -> D::Block {
+        if d.data(b).height > d.data(ep).height {
             b
         } else {
             ep
         }
     }
 
-    fn tip<D: BlockDAG>(&self, d: D, ep: D::Block) -> D::Block {
+    fn tip<D: BlockDAG<Data>>(&self, d: D, ep: D::Block) -> D::Block {
         ep
     }
 
-    fn pred<D: BlockDAG>(&self, d: D, b: D::Block) -> D::Block {
-        d.parents(b).next().unwrap()
+    fn pred<D: BlockDAG<Data>>(&self, d: D, b: D::Block) -> Option<D::Block> {
+        d.parents(b).next()
     }
 
-    fn progress<D: BlockDAG>(&self, d: D, b: D::Block) -> f32 {
+    fn progress<D: BlockDAG<Data>>(&self, d: D, b: D::Block) -> f32 {
         1.
     }
 
-    type RewardAlloc<D: BlockDAG> = Once<(D::Miner, f32)>;
+    type RewardAlloc<D: BlockDAG<Data>> = Once<(D::Miner, f32)>;
 
-    fn reward<D: BlockDAG>(&self, d: D, b: D::Block) -> Self::RewardAlloc<D> {
+    fn reward<D: BlockDAG<Data>>(&self, d: D, b: D::Block) -> Self::RewardAlloc<D> {
         once((d.miner(b), 1.))
     }
 }
