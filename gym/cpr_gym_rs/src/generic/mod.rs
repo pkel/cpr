4a15f56743c99819d1dd8cfba2dbb752a5c4cf84
Patrik Keller <git@pkel.dev>
2024-03-30 12:34:18 +0100

    Action encoding

--- gym/cpr_gym_rs/src/generic/mod.rs
+++ gym/cpr_gym_rs/src/generic/mod.rs
@@ -42,8 +42,9 @@ enum NView {
 
 use petgraph;
 
-type BlockIx = petgraph::graph::DefaultIx;
-type Block = petgraph::graph::NodeIndex<BlockIx>;
+type NodeIx = petgraph::graph::DefaultIx;
+type Node = petgraph::graph::NodeIndex<NodeIx>;
+type Block = Node;
 
 struct NodeWeight<P> {
     av: AView,
@@ -54,11 +55,12 @@ struct NodeWeight<P> {
 
 type EdgeWeight = ();
 
-type BlockDAG<ProtoData> = petgraph::graph::DiGraph<NodeWeight<ProtoData>, EdgeWeight>;
+type Graph<ProtoData> = petgraph::graph::DiGraph<NodeWeight<ProtoData>, EdgeWeight>;
 
 pub mod intf;
+use intf::BlockDAG;
 
-impl<'a, ProtoData> intf::BlockDAG<Block, Party, ProtoData> for &'a BlockDAG<ProtoData> {
+impl<'a, ProtoData> BlockDAG<Block, Party, ProtoData> for &'a Graph<ProtoData> {
     fn parents(&self, b: Block) -> Vec<Block> {
         self.neighbors_directed(b, petgraph::Direction::Outgoing)
             .collect()
@@ -112,19 +114,19 @@ fn bflt_a_avail<P>(nd: &NodeWeight<P>) -> bool {
 
 // The BlockDAG is subject to some invariants, which I check below.
 
-fn dag_check<ProtoData>(dag: BlockDAG<ProtoData>) {
-    assert!(dag.node_count() > 0, "dag is empty");
+fn dag_check<ProtoData>(g: Graph<ProtoData>) {
+    assert!(g.node_count() > 0, "dag is empty");
     assert!(
-        petgraph::algo::connected_components(&dag) == 1,
+        petgraph::algo::connected_components(&g) == 1,
         "dag not connected"
     );
-    assert!(!petgraph::algo::is_cyclic_directed(&dag), "not a dag");
+    assert!(!petgraph::algo::is_cyclic_directed(&g), "not a dag");
 
     let mut aentry = 0;
     let mut dentry = 0;
 
-    for n in dag.node_indices() {
-        let nd = dag.node_weight(n).unwrap();
+    for n in g.node_indices() {
+        let nd = g.node_weight(n).unwrap();
 
         // count entry points, there should be one each
         if nd.av == AView::AEntry {
@@ -136,12 +138,12 @@ fn dag_check<ProtoData>(dag: BlockDAG<ProtoData>) {
 
         // check topological visibility
         if bflt_a_sees(nd) {
-            let f = |p| -> _ { bflt_a_sees(dag.node_weight(p).unwrap()) };
-            assert!(dag.neighbors(n).all(f), "topological visibility")
+            let f = |p| -> _ { bflt_a_sees(g.node_weight(p).unwrap()) };
+            assert!(g.neighbors(n).all(f), "topological visibility")
         }
         if bflt_d_sees(nd) {
-            let f = |p| -> _ { bflt_d_sees(dag.node_weight(p).unwrap()) };
-            assert!(dag.neighbors(n).all(f), "topological visibility")
+            let f = |p| -> _ { bflt_d_sees(g.node_weight(p).unwrap()) };
+            assert!(g.neighbors(n).all(f), "topological visibility")
         }
     }
 
@@ -152,55 +154,125 @@ fn dag_check<ProtoData>(dag: BlockDAG<ProtoData>) {
 // Partial views on the DAG
 
 struct AttackerView<'a, ProtoData> {
-    dag: &'a BlockDAG<ProtoData>,
+    g: &'a Graph<ProtoData>,
 }
 
-impl<'a, ProtoData> intf::BlockDAG<Block, Party, ProtoData> for &'a AttackerView<'a, ProtoData> {
+impl<'a, ProtoData> BlockDAG<Block, Party, ProtoData> for &'a AttackerView<'a, ProtoData> {
     fn parents(&self, b: Block) -> Vec<Block> {
         // parents are always visible, so we do not filter
-        self.dag.parents(b)
+        self.g.parents(b)
     }
 
     fn children(&self, b: Block) -> Vec<Block> {
-        let f = |x: &Block| self.dag.node_weight(*x).unwrap().av != AView::Ignored;
-        self.dag
+        let f = |x: &Block| self.g.node_weight(*x).unwrap().av != AView::Ignored;
+        self.g
             .neighbors_directed(b, petgraph::Direction::Incoming)
             .filter(f)
             .collect()
     }
 
     fn miner(&self, b: Block) -> Party {
-        self.dag.miner(b)
+        self.g.miner(b)
     }
 
     fn data(&self, b: Block) -> &ProtoData {
-        self.dag.data(b)
+        self.g.data(b)
     }
 }
 
 struct DefenderView<'a, ProtoData> {
-    dag: &'a BlockDAG<ProtoData>,
+    g: &'a Graph<ProtoData>,
 }
 
-impl<'a, ProtoData> intf::BlockDAG<Block, Party, ProtoData> for &'a DefenderView<'a, ProtoData> {
+impl<'a, ProtoData> BlockDAG<Block, Party, ProtoData> for &'a DefenderView<'a, ProtoData> {
     fn parents(&self, b: Block) -> Vec<Block> {
         // parents are always visible, so we do not filter
-        self.dag.parents(b)
+        self.g.parents(b)
     }
 
     fn children(&self, b: Block) -> Vec<Block> {
-        let f = |x: &Block| self.dag.node_weight(*x).unwrap().dv != DView::Unknown;
-        self.dag
+        let f = |x: &Block| self.g.node_weight(*x).unwrap().dv != DView::Unknown;
+        self.g
             .neighbors_directed(b, petgraph::Direction::Incoming)
             .filter(f)
             .collect()
     }
 
     fn miner(&self, b: Block) -> Party {
-        self.dag.miner(b)
+        self.g.miner(b)
     }
 
     fn data(&self, b: Block) -> &ProtoData {
-        self.dag.data(b)
+        self.g.data(b)
     }
+}
+
+// Action Space
+//
+// The attacker may
+//  - withhold own blocks, i.e. not release them to the defender, and
+//  - ignore any block, i.e. not consider them when extending the chain.
+//
+// We impose some topological restrictions. These follow from the topological restrictions in the
+// DAG views. In practice, it follows from the natural restriction that verifying hash references
+// requires availability of all parents blocks. Blocks must be
+//   - released after their parents, and
+//   - considered after their parents.
+//
+// When looking at a regular protocol execution, most blocks will be released and considered
+// already. Even under attack, only a few blocks are 'actionable'. E.g., in Bitcoin, when looking
+// at a period where the attacker maintains a long private fork, at most three blocks are
+// actionable (the first ignored/ignored/withheld defender/attacker/attacker block can be
+// considered/considered/released).
+//
+// We thus expose action of the form
+//  - `Release i`. Among all blocks that can be released, release the i-th option.
+//  - `Consider i`. Among all blocks that can be considered, consider the i-th option.
+//
+// The release action merely flips `AView` enum of the block.
+// A third action `Continue` triggers the next simulation step, which
+//  - (a) concludes the previous communication between the defending nodes
+//  - (b) sends the to be released blocks to the defending nodes
+//  - (c) advances in time until the next block is mined.
+// Steps (a) and (b) might happen in reverse order depending on the network assumptions.
+//
+// We encode the actions as signed integers:
+//
+// - negative integers: -i means `Release  i`
+// - positive integers:  i means `Consider i`
+// - zero:               0 means `Continue`
+
+struct AvailableActions {
+    release: Vec<Block>,
+    consider: Vec<Block>,
+}
+
+fn available_actions<P>(g: &Graph<P>) -> AvailableActions {
+    let mut release = vec![];
+    let mut consider = vec![];
+
+    // iterate all blocks
+    for b in g.node_indices() {
+        let nw = g.node_weight(b).unwrap();
+
+        // check release possible
+        if nw.nv == NView::Withheld
+            && g.parents(b)
+                .into_iter()
+                .all(|p| g.node_weight(b).unwrap().nv != NView::Withheld)
+        {
+            release.push(b);
+        }
+
+        // check consider possible
+        if nw.av == AView::Ignored
+            && g.parents(b)
+                .into_iter()
+                .all(|p| g.node_weight(b).unwrap().av != AView::Ignored)
+        {
+            consider.push(b);
+        }
+    }
+
+    AvailableActions { release, consider }
 }
