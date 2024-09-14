71800251726b3aa804fe611643e99fc44a8a6a9a
Patrik Keller <git@pkel.dev>
2024-09-14 12:43:03 +0200

    generic_v1. track listings

---
+++ mdp/lib/models/generic_v1/listings/bitcoin.py
@@ -0,0 +1,34 @@
+def init(state):
+    state.head = genesis
+
+
+def mining(state):
+    return [state.head]
+
+
+def update(state, block):
+    if height(block) > height(state.head):
+        state.head = block
+
+
+def height(block):
+    return len(history_of(block))
+
+
+def history_of(block):
+    if block == genesis:
+        return [genesis]
+    else:
+        return history_of(parents(block)[0]) + [block]
+
+
+def history(state):
+    return history_of(state.head)
+
+
+def progress(_block):
+    return 1
+
+
+def coinbase(block):
+    return [(miner_of(block), 1)]
---
+++ mdp/lib/models/generic_v1/listings/ghostdag_util.py
@@ -0,0 +1,50 @@
+k = 3
+
+## omitted, boring helper functions
+
+
+def tips(subgraph):
+    return {b for b in subgraph if len(children(b) & subgraph) == 0}
+
+
+def past_or_future(relation, subgraph, block):
+    acc = set()
+    stack = set(relation(block)) & subgraph
+    while len(stack) > 0:
+        b = stack.pop()
+        if b not in acc:
+            acc.add(b)
+            for p in set(relation(b)) & subgraph:
+                stack.add(p)
+    return acc
+
+
+def past(subgraph, block):
+    return past_or_future(parents, subgraph, block)
+
+
+def future(subgraph, block):
+    return past_or_future(children, subgraph, block)
+
+
+def anticone(subgraph, block):
+    return subgraph - {block} - past(subgraph, block) - future(subgraph, block)
+
+
+## omitted, boring protocol spec functions
+
+
+def init(_state):
+    pass
+
+
+def update(_state, _block):
+    pass
+
+
+def progress(_block):
+    return 1
+
+
+def coinbase(block):
+    return [(miner_of(block), 1)]
