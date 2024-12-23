pkel/cpr

commit 65d04b9174f26003bc34b2bda2f889bae48eebce

generic_v1. miner_of(genesis) is not defined
Why does this cause a change in model size?

mdp/lib/models/generic_v1/model.py
@@ -49,8 +49,11 @@ class DAG:
         if not self._frozen:
             raise AttributeError("mutable DAG; use .freeze() first")
 
+        assert self._parents[0] == set()
+        assert self._miner[0] is None
+
         x = xxhash.xxh128()
-        for i, parents in enumerate(self._parents):
+        for i, parents in enumerate(self._parents[1:]):
             x.update(f"{i},{self._miner[i]},")
             for j, p in enumerate(parents):
                 x.update(f"{j},{p},")
@@ -101,6 +104,7 @@ class DAG:
             return self._children[block] & subgraph
 
     def miner_of(self, block: int) -> int:
+        assert block != self.genesis, "unsafe usage of miner_of"
         return self._miner[block]
 
     def height(self, block: int) -> int:
@@ -626,7 +630,7 @@ class SingleAgentImp:
             (
                 self._dag.height(b),
                 new_positions_of_old_blocks[b],
-                self._dag.miner_of(b),
+                self._dag.miner_of(b) if b != self._dag.genesis else -1,
                 b,
             )
             for b in sorted(self._dag.all_blocks())
@@ -747,6 +751,8 @@ class SingleAgent(ImplicitMDP):
             raise ValueError("reward_common_chain requires truncate_common_chain")
 
         self.start_state = SingleAgentImp(protocol, *args, **kwargs)
+        if merge_isomorphic:
+            self.start_state = self.start_state.copy_and_normalize()
         self.start_state.freeze()
 
         if self.loop_honest:

mdp/lib/policy_guided_explorer_test.py
@@ -126,22 +126,22 @@ def test_v1_bitcoin():
 
     # distance 0
     m = explorer.mdp()
-    assert m.n_states == 6
+    assert m.n_states == 5
 
     # distance 1
     explorer.explore_aside_policy()
     m = explorer.mdp()
-    assert m.n_states == 27
+    assert m.n_states == 24
 
     # distance 2
     explorer.explore_aside_policy()
     m = explorer.mdp()
-    assert m.n_states == 185
+    assert m.n_states == 169
 
     # distance 3
     # explorer.explore_aside_policy()
     # m = explorer.mdp()
-    # assert m.n_states == 1189
+    # assert m.n_states == 1081
 
 
 def test_v1_ghostdag3():
@@ -154,19 +154,19 @@ def test_v1_ghostdag3():
 
     # distance 0
     m = explorer.mdp()
-    assert m.n_states == 6
+    assert m.n_states == 5
 
     # distance 1
     explorer.explore_aside_policy()
     m = explorer.mdp()
-    assert m.n_states == 32
+    assert m.n_states == 23
 
     # distance 2
     explorer.explore_aside_policy()
     m = explorer.mdp()
-    assert m.n_states == 264
+    assert m.n_states == 197
 
     # distance 3
     # explorer.explore_aside_policy()
     # m = explorer.mdp()
-    # assert m.n_states == 2185
+    # assert m.n_states == 1585

mdp/lib/models/generic_v1/sim.py
@@ -16,7 +16,7 @@ class SingleMinerSim:
     def reward_and_progress(self):
         history = self.miner.history()
         rew, prg = 0, 0
-        for b in history:
+        for b in history[1:]:  # skip genesis block
             for _, amount in self.miner.coinbase(b):
                 rew += amount
             prg += self.miner.progress(b)
@@ -108,7 +108,7 @@ class NetworkSim(DiscreteEventSim):
     def reward_and_progress(self):
         history = self.judge.history()
         rew, prg = 0, 0
-        for b in history:
+        for b in history[1:]:  # skip genesis block
             for _, amount in self.judge.coinbase(b):
                 rew += amount
             prg += self.judge.progress(b)

mdp/lib/models/generic_v1/test/test_listings.py
@@ -46,7 +46,7 @@ def load_protocol(name, dag, state):
 def reward_and_progress(miner):
     history = miner.history()
     rew, prg = 0, 0
-    for b in history:
+    for b in history[1:]:  # skip genesis block
         for _, amount in miner.coinbase(b):
             rew += amount
         prg += miner.progress(b)

commit 0e5d4375ff0a7f03cb09dd533d69315516e050df

generic_v1. fix state fingerprinting bug
introduced in 65d04b9174. This was causing non-isomorphic states being
merged

mdp/lib/models/generic_v1/model.py
@@ -53,10 +53,10 @@ class DAG:
         assert self._miner[0] is None
 
         x = xxhash.xxh128()
-        for i, parents in enumerate(self._parents[1:]):
-            x.update(f"{i},{self._miner[i]},")
-            for j, p in enumerate(sorted(parents)):
-                x.update(f"{j},{p},")
+        for i in range(1, len(self._parents)):  # skip genesis
+            x.update(f";{i},{self._miner[i]}")
+            for p in sorted(self._parents[i]):
+                x.update(f",{p}")
 
         return x.digest()
 
@@ -158,7 +158,7 @@ class DynObj:
 
         x = xxhash.xxh128()
         for i, (k, v) in enumerate(sorted(self._attributes.items())):
-            x.update(f"{i},{k},{v}")
+            x.update(f";{i},{k},{v}")
 
         return x.digest()
 
@@ -232,7 +232,7 @@ class Miner:
 
         x = xxhash.xxh128()
         for i, b in enumerate(sorted(self._visible)):
-            x.update(f"{i},{b},")
+            x.update(f";{i},{b}")
         x.update(self._protocol.state.fingerprint())
 
         return x.digest()
@@ -384,11 +384,11 @@ class SingleAgentImp:
         x.update(self._dag.fingerprint())
         x.update(self._attacker.fingerprint())
         x.update(self._defender.fingerprint())
-        for i, b in enumerate(sorted(self._withheld)):
-            x.update(f"{i},{b}")
-        x.update("|")
-        for i, b in enumerate(sorted(self._ignored)):
-            x.update(f"{i},{b}")
+        for b in sorted(self._withheld):
+            x.update(f",{b}")
+        x.update(";")
+        for b in sorted(self._ignored):
+            x.update(f",{b}")
 
         return x.digest()
 

mdp/lib/policy_guided_explorer_test.py
@@ -15,34 +15,19 @@ def test_aft20barzur():
 
     explorer = Explorer(model, model.honest)
 
+    n_states = []
+
     # distance 0
     m = explorer.mdp()
-    assert m.n_states == 4
+    n_states.append(m.n_states)
 
-    # distance 1
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 9
+    # distance 1...
+    for _ in range(5):
+        explorer.explore_aside_policy()
+        m = explorer.mdp()
+        n_states.append(m.n_states)
 
-    # distance 2
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 16
-
-    # distance 3
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 26
-
-    # distance 4
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 38
-
-    # distance 5
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 53
+    assert n_states == [4, 9, 16, 26, 38, 53]
 
 
 def test_fc16sapirshtein():
@@ -53,34 +38,19 @@ def test_fc16sapirshtein():
 
     explorer = Explorer(model, model.honest)
 
+    n_states = []
+
     # distance 0
     m = explorer.mdp()
-    assert m.n_states == 4
+    n_states.append(m.n_states)
 
-    # distance 1
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 8
+    # distance 1...
+    for _ in range(5):
+        explorer.explore_aside_policy()
+        m = explorer.mdp()
+        n_states.append(m.n_states)
 
-    # distance 2
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 15
-
-    # distance 3
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 24
-
-    # distance 4
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 36
-
-    # distance 5
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 50
+    assert n_states == [4, 8, 15, 24, 36, 50]
 
 
 def test_v0_bitcoin():
@@ -91,29 +61,19 @@ def test_v0_bitcoin():
 
     explorer = Explorer(model, model.honest)
 
+    n_states = []
+
     # distance 0
     m = explorer.mdp()
-    assert m.n_states == 11
+    n_states.append(m.n_states)
 
-    # distance 1
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 37
+    # distance 1...
+    for _ in range(4):
+        explorer.explore_aside_policy()
+        m = explorer.mdp()
+        n_states.append(m.n_states)
 
-    # distance 2
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 111
-
-    # distance 3
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 279
-
-    # distance 4
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 601
+    assert n_states == [11, 37, 111, 279, 601]
 
 
 def test_v1_bitcoin():
@@ -124,24 +84,19 @@ def test_v1_bitcoin():
 
     explorer = Explorer(model, model.honest)
 
+    n_states = []
+
     # distance 0
     m = explorer.mdp()
-    assert m.n_states == 5
+    n_states.append(m.n_states)
 
-    # distance 1
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 24
+    # distance 1...
+    for _ in range(2):
+        explorer.explore_aside_policy()
+        m = explorer.mdp()
+        n_states.append(m.n_states)
 
-    # distance 2
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 169
-
-    # distance 3
-    # explorer.explore_aside_policy()
-    # m = explorer.mdp()
-    # assert m.n_states == 1081
+    assert n_states == [6, 27, 185]
 
 
 def test_v1_ghostdag3():
@@ -152,24 +107,19 @@ def test_v1_ghostdag3():
 
     explorer = Explorer(model, model.honest)
 
+    n_states = []
+
     # distance 0
     m = explorer.mdp()
-    assert m.n_states == 5
+    n_states.append(m.n_states)
 
-    # distance 1
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 23
+    # distance 1...
+    for _ in range(2):
+        explorer.explore_aside_policy()
+        m = explorer.mdp()
+        n_states.append(m.n_states)
 
-    # distance 2
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 197
-
-    # distance 3
-    # explorer.explore_aside_policy()
-    # m = explorer.mdp()
-    # assert m.n_states == 1585
+    assert n_states == [6, 32, 264]
 
 
 def test_v1_bitcoin_tcc():
@@ -184,19 +134,19 @@ def test_v1_bitcoin_tcc():
 
     explorer = Explorer(model, model.honest)
 
+    n_states = []
+
     # distance 0
     m = explorer.mdp()
-    assert m.n_states == 6
+    n_states.append(m.n_states)
 
-    # distance 1
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 27
+    # distance 1...
+    for _ in range(2):
+        explorer.explore_aside_policy()
+        m = explorer.mdp()
+        n_states.append(m.n_states)
 
-    # distance 2
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 152
+    assert n_states == [7, 30, 171]
 
 
 def test_v1_ghostdag3_tcc():
@@ -212,19 +162,19 @@ def test_v1_ghostdag3_tcc():
 
     explorer = Explorer(model, model.honest)
 
+    n_states = []
+
     # distance 0
     m = explorer.mdp()
-    assert m.n_states == 6
+    n_states.append(m.n_states)
 
-    # distance 1
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 31
+    # distance 1...
+    for _ in range(2):
+        explorer.explore_aside_policy()
+        m = explorer.mdp()
+        n_states.append(m.n_states)
 
-    # distance 2
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 219
+    assert n_states == [7, 41, 305]
 
 
 def test_v1_bitcoin_mi():
@@ -240,19 +190,19 @@ def test_v1_bitcoin_mi():
 
     explorer = Explorer(model, model.honest)
 
+    n_states = []
+
     # distance 0
     m = explorer.mdp()
-    assert m.n_states == 6
+    n_states.append(m.n_states)
 
-    # distance 1
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 34
+    # distance 1...
+    for _ in range(2):
+        explorer.explore_aside_policy()
+        m = explorer.mdp()
+        n_states.append(m.n_states)
 
-    # distance 2
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 185
+    assert n_states == [7, 41, 227]
 
 
 def test_v1_ghostdag3_mi():
@@ -269,16 +219,16 @@ def test_v1_ghostdag3_mi():
 
     explorer = Explorer(model, model.honest)
 
+    n_states = []
+
     # distance 0
     m = explorer.mdp()
-    assert m.n_states == 6
+    n_states.append(m.n_states)
 
-    # distance 1
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 28
+    # distance 1...
+    for _ in range(2):
+        explorer.explore_aside_policy()
+        m = explorer.mdp()
+        n_states.append(m.n_states)
 
-    # distance 2
-    explorer.explore_aside_policy()
-    m = explorer.mdp()
-    assert m.n_states == 164
+    assert n_states == [7, 37, 230]